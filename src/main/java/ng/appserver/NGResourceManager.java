59af66f61b4bb632ede46a0a45a9b2458877d96f
Hugi Thordarson <hugi@karlmenn.is>
2021-04-25 12:33:03 +0000

    Use NGUtils to read the java resource

--- src/main/java/ng/appserver/NGResourceManager.java
+++ src/main/java/ng/appserver/NGResourceManager.java
@@ -1,11 +1,11 @@
 package ng.appserver;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ng.appserver.privates.NGUtils;
 
 /**
  * Experimental implementation of the resource manager
@@ -23,16 +23,6 @@ public class NGResourceManager {
 		
 		logger.info( "Loading resource {} at path {}", resourceName, resourcePath );
 
-		try( final InputStream resourceAsStream = NGResourceManager.class.getResourceAsStream( resourcePath )) {
-
-			if( resourceAsStream == null ) {
-				return Optional.empty();
-			}
-
-			return Optional.of( resourceAsStream.readAllBytes() );
-		}
-		catch( IOException e ) {
-			throw new RuntimeException( e );
-		}
+		return NGUtils.readJavaResource( resourceName );
 	}
 }
