c221ce01ef16c21e47834de3589701786bdd7f2c
Hugi Thordarson <hugi@karlmenn.is>
2021-04-25 13:21:30 +0000

    Add experimental resource cache

--- src/main/java/ng/appserver/NGResourceManager.java
+++ src/main/java/ng/appserver/NGResourceManager.java
@@ -1,5 +1,7 @@
 package ng.appserver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -15,11 +17,32 @@ public class NGResourceManager {
 
 	private static final Logger logger = LoggerFactory.getLogger( NGResourceManager.class );
 
+	/**
+	 * FIXME: Experimental cache
+	 */
+	private Map<String,Optional<byte[]>> _cache = new HashMap<>();
+
 	public Optional<byte[]> bytesForResourceNamed( final String resourceName ) {
 		final String actualResourcePath = "/app-resources/" + resourceName;
 
 		logger.info( "Loading resource {} from {}", resourceName, actualResourcePath );
 
-		return NGUtils.readJavaResource( actualResourcePath );
+		if( useCache() ) {
+			Optional<byte[]> resource = _cache.get( resourceName );
+			
+			if( resource == null ) {
+				resource = NGUtils.readJavaResource( actualResourcePath );
+				_cache.put( resourceName, resource );
+			}
+			
+			return resource;
+		}
+		else {
+			return NGUtils.readJavaResource( actualResourcePath );
+		}
+	}
+	
+	private boolean useCache() {
+		return false;
 	}
 }
