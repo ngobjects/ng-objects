95b4af5002cbc2b5d102c74b04e60029fbad6efd
Hugi Thordarson <hugi@karlmenn.is>
2021-04-25 10:31:34 +0000

    Added NGURIParser

---
+++ src/main/java/ng/appserver/privates/NGURIParser.java
@@ -0,0 +1,39 @@
+package ng.appserver.privates;
+
+import java.util.Objects;
+
+public class NGURIParser {
+
+	final String _sourceUri;
+	
+	public NGURIParser( final String sourceUri ) {
+		Objects.requireNonNull( sourceUri );
+		_sourceUri = sourceUri;
+	}
+	
+	public String[] elements() {
+		String uri = _sourceUri;
+
+		if( uri.startsWith( "/" ) ) {
+			uri = uri.substring( 1 );
+		}
+
+		if( uri.length() == 0 ) {
+			return new String[] {};
+		}
+
+		return uri.substring(0).split( "/" );
+	}
+	
+	/**
+	 * FIXME: This should probably return an Optional at some point.
+	 */
+	public String elementAt( final int i ) {
+
+		if( i > elements().length ) {
+			return null;
+		}
+		
+		return elements()[i];
+	}
+}
