11a30b00bb9b0ee7b35b1604b41207f83448cc48
Hugi Thordarson <hgui@karlmenn.is>
2021-07-06 08:31:24 +0000

    Include a placeholder dispatchRequest method

--- src/main/java/ng/appserver/NGAdaptor.java
+++ src/main/java/ng/appserver/NGAdaptor.java
@@ -3,4 +3,11 @@ package ng.appserver;
 public abstract class NGAdaptor {
 
 	public abstract void start();
+
+	/**
+	 * FIXME: Added as a placeholder until I decide the best way to pass the request on to the actual request handling mechanism
+	 */
+	public NGResponse dispatchRequest( final NGRequest request ) {
+		return NGApplication.application().dispatchRequest( request );
+	}
 }
