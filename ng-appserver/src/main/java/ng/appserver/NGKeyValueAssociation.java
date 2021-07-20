5c7af7de3c385c51e544ef0064b317d76fd98d82
Hugi Thordarson <hugi@karlmenn.is>
2021-07-20 21:05:59 +0000

    Skeleton implementation of NGKeyValueAssociation

--- ng-appserver/src/main/java/ng/appserver/NGKeyValueAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/NGKeyValueAssociation.java
@@ -1,5 +1,17 @@
 package ng.appserver;
 
+import ng.kvc.NGKeyValueCoding;
+
 public class NGKeyValueAssociation extends NGAssociation {
 
+	private final String _keyPath;
+
+	public NGKeyValueAssociation( final String keyPath ) {
+		_keyPath = keyPath;
+	}
+
+	@Override
+	public Object valueInComponent( final NGComponent aComponent ) {
+		return NGKeyValueCoding.Utility.valueForKeyPath( aComponent, _keyPath );
+	}
 }
