0878ba8440d4b136bb792b376515c05f9d0cce47
Hugi Thordarson <hugi@karlmenn.is>
2021-07-30 08:56:05 +0000

    Add sessionID/birthDate

--- ng-appserver/src/main/java/ng/appserver/NGSession.java
+++ ng-appserver/src/main/java/ng/appserver/NGSession.java
@@ -1,10 +1,16 @@
 package ng.appserver;
 
 /**
- * Yes, we have sessions too! 
+ * Yes, we have sessions too!
  */
 
 public class NGSession {
 
-	
+	final String _sessionID;
+	final long _birthDate;
+
+	public NGSession( final String sessionID ) {
+		_sessionID = sessionID;
+		_birthDate = System.currentTimeMillis();
+	}
 }
