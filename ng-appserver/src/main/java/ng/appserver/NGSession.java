302a2bbf12bb6d16266cc11ff09e4d21aedd9690
Hugi Thordarson <hugi@karlmenn.is>
2021-11-13 13:12:56 +0000

    Don't require sessionID in the session constructor

--- ng-appserver/src/main/java/ng/appserver/NGSession.java
+++ ng-appserver/src/main/java/ng/appserver/NGSession.java
@@ -1,5 +1,7 @@
 package ng.appserver;
 
+import java.util.UUID;
+
 /**
  * Yes, we have sessions too!
  */
@@ -9,8 +11,16 @@ public class NGSession {
 	final String _sessionID;
 	final long _birthDate;
 
+	public NGSession() {
+		this( UUID.randomUUID().toString() );
+	}
+
 	public NGSession( final String sessionID ) {
+		this( sessionID, System.currentTimeMillis() );
+	}
+
+	private NGSession( final String sessionID, long birthDate ) {
 		_sessionID = sessionID;
-		_birthDate = System.currentTimeMillis();
+		_birthDate = birthDate;
 	}
 }
