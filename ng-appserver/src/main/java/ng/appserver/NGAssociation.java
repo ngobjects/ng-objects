b5eabf36bab38c2ceb1c07488f500cce3d1ff744
Hugi Thordarson <hugi@karlmenn.is>
2021-11-13 15:08:41 +0000

    Did not add logging of pulling and pushing values for NGAssociation

--- ng-appserver/src/main/java/ng/appserver/NGAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/NGAssociation.java
@@ -1,11 +1,29 @@
 package ng.appserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class NGAssociation {
+
+	private static final Logger logger = LoggerFactory.getLogger( NGAssociation.class );
+
+	/**
+	 * This boolean is added since it's used by WOOgnl. Definitely want to remove this // FIXME Hugi 2021-11-13
+	 */
+	protected boolean _debugEnabled = true;
 
 	public Object valueInComponent( NGComponent aComponent ) {
 		return null;
 	}
 
 	public void setValue( Object aValue, NGComponent aComponent ) {
+	}
+
+	protected void _logPullValue( Object aValue, NGComponent aComponent ) {
+		logger.error( "Not implemented" );
+	}
+
+	protected void _logPushValue( Object aValue, NGComponent aComponent ) {
+		logger.error( "Not implemented" );
 	}
 }
