67d11bed730bc9ce218ad1cc0d7677ccc5924b57
Hugi Thordarson <hugi@karlmenn.is>
2021-11-13 15:03:22 +0000

    Added a keyPath() cover method

--- ng-appserver/src/main/java/ng/appserver/NGKeyValueAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/NGKeyValueAssociation.java
@@ -12,7 +12,11 @@ public class NGKeyValueAssociation extends NGAssociation {
 
 	@Override
 	public Object valueInComponent( final NGComponent aComponent ) {
-		// FIXME: We're probably going to want to use valueForKeyPath here // Hugi 2021-10-08 
-		return NGKeyValueCoding.Utility.valueForKey( aComponent, _keyPath );
+		// FIXME: We're probably going to want to use valueForKeyPath here // Hugi 2021-10-08
+		return NGKeyValueCoding.Utility.valueForKey( aComponent, keyPath() );
+	}
+
+	public String keyPath() {
+		return _keyPath;
 	}
 }
