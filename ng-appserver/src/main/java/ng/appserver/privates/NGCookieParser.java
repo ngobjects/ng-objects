9a3a318676a02a29fd1423bd7334c35a1e0cad55
Hugi Thordarson <hugi@karlmenn.is>
2021-11-16 09:44:44 +0000

    Add the cookie parser skeleton and related tests

---
+++ ng-appserver/src/main/java/ng/appserver/privates/NGCookieParser.java
@@ -0,0 +1,12 @@
+package ng.appserver.privates;
+
+import java.util.List;
+
+import ng.appserver.NGCookie;
+
+public class NGCookieParser {
+
+	public List<NGCookie> cookies( final String cookieHeader ) {
+		return List.of();
+	}
+}
---
+++ ng-appserver/src/test/java/ng/appserver/privates/TestNGCookieParser.java
@@ -0,0 +1,11 @@
+package ng.appserver.privates;
+
+import org.junit.jupiter.api.Test;
+
+public class TestNGCookieParser {
+
+	@Test
+	public void parseSingle() {
+
+	}
+}
