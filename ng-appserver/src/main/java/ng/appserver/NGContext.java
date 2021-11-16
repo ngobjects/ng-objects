95879b492a6008ed04f2649642c2ef30d2968d1c
Hugi Thordarson <hugi@karlmenn.is>
2021-11-16 14:36:07 +0000

    Cleanup

--- ng-appserver/src/main/java/ng/appserver/NGContext.java
+++ ng-appserver/src/main/java/ng/appserver/NGContext.java
@@ -15,12 +15,12 @@ public class NGContext {
 		_request = request;
 	}
 
-	public NGResponse response() {
-		return _response;
-	}
-
 	public NGRequest request() {
 		return _request;
+	}
+
+	public NGResponse response() {
+		return _response;
 	}
 
 	public NGSession session() {
