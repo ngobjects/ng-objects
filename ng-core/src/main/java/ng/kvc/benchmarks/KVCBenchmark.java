79f84648966e950f9077ab73229cf7976450c674
Hugi Thordarson <hugi@karlmenn.is>
2021-11-20 14:52:15 +0000

    Added a performance benchmark. Which seems kind of… Redundant

---
+++ ng-core/src/main/java/ng/kvc/benchmarks/KVCBenchmark.java
@@ -0,0 +1,28 @@
+package ng.kvc.benchmarks;
+
+import ng.kvc.NGKeyValueCoding;
+
+public class KVCBenchmark {
+
+	public static void main( String[] argv ) {
+		Person person = new Person();
+		person._name = "Hugi";
+
+		int numberOfRuns = 10_000_000;
+
+		long start = System.currentTimeMillis();
+		for( int i = 0; i < numberOfRuns; i++ ) {
+			Object valueForKey = NGKeyValueCoding.Utility.valueForKey( person, "name" );
+		}
+
+		System.out.println( "Time: " + (System.currentTimeMillis() - start) );
+	}
+
+	public static class Person {
+		public String _name;
+
+		public String name() {
+			return _name;
+		}
+	}
+}
