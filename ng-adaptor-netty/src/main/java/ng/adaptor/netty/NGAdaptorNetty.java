85dff9703d0b910f40bafedd37ab6a1e39f9936e
Hugi Thordarson <hugi@karlmenn.is>
2021-11-20 11:37:30 +0000

    Added scaffolding for the Netty adaptor

---
+++ ng-adaptor-netty/src/main/java/ng/adaptor/netty/NGAdaptorNetty.java
@@ -0,0 +1,15 @@
+package ng.adaptor.netty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ng.appserver.NGAdaptor;
+
+public class NGAdaptorNetty extends NGAdaptor {
+
+	private static final Logger logger = LoggerFactory.getLogger( NGAdaptorNetty.class );
+
+	@Override
+	public void start() {
+	}
+}
