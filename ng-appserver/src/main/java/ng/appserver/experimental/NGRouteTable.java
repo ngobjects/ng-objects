6187aef30cfcd00ac7deb3552061d7b7de43a0b9
Hugi Thordarson <hugi@karlmenn.is>
2021-11-21 16:34:16 +0000

    Experimental route table

---
+++ ng-appserver/src/main/java/ng/appserver/experimental/NGRouteTable.java
@@ -0,0 +1,159 @@
+package ng.appserver.experimental;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ng.appserver.NGActionResults;
+import ng.appserver.NGApplication;
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+import ng.appserver.NGRequest;
+import ng.appserver.NGResponse;
+
+/**
+ * Contains a list of handlers for URLs
+ *
+ * Routing allows a couple of types of wildcards
+ *
+ * - Named parameters are prefixed with a colon
+ * - Positional parameters are a star
+ */
+
+public class NGRouteTable {
+
+	private static final NotFoundRouteHandler NOT_FOUND_ROUTE_HANDLER = new NotFoundRouteHandler();
+
+	private static final Logger logger = LoggerFactory.getLogger( NGRouteTable.class );
+
+	/**
+	 * A list of all routes mapped by this table
+	 */
+	private List<Route> _routes = new ArrayList<>();
+
+	/**
+	 * The default global route table used by RouteAction to access actions
+	 */
+	private static NGRouteTable _defaultRouteTable = new NGRouteTable();
+
+	public static NGRouteTable defaultRouteTable() {
+		return _defaultRouteTable;
+	}
+
+	private List<Route> routes() {
+		return _routes;
+	}
+
+	private RouteHandler handlerForURL( final WrappedURL url ) {
+
+		for( final Route route : routes() ) {
+			if( matches( route.pattern, url.sourceURL() ) ) {
+				return route.routeHandler;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Check if the given handler matches the given URL
+	 */
+	private static boolean matches( final String pattern, final String url ) {
+		return url.startsWith( pattern );
+	}
+
+	/**
+	 * Handle the given URL
+	 */
+	public NGActionResults handle( final WrappedURL url, final NGContext context ) {
+		final NGRequest request = context.request();
+		logger.info( "Handling URL: {}", url );
+		RouteHandler routeHandler = handlerForURL( url );
+
+		if( routeHandler == null ) {
+			logger.warn( "No RouteHandler found for URL: {}", url.toString() );
+			routeHandler = NOT_FOUND_ROUTE_HANDLER;
+		}
+
+		return routeHandler.handle( url, context );
+	}
+
+	public void map( final String pattern, final RouteHandler routeHandler ) {
+		Route r = new Route();
+		r.pattern = pattern;
+		r.routeHandler = routeHandler;
+		_routes.add( r );
+	}
+
+	public void map( final String pattern, final BiFunction<WrappedURL, NGContext, NGActionResults> biFunction ) {
+		final BiFunctionRouteHandler routeHandler = new BiFunctionRouteHandler( biFunction );
+		map( pattern, routeHandler );
+	}
+
+	public void mapComponent( final String pattern, final Class<? extends NGComponent> componentClass ) {
+		final ComponentRouteHandler routeHandler = new ComponentRouteHandler( componentClass );
+		map( pattern, routeHandler );
+	}
+
+	/**
+	 * Maps a URL pattern to a given RouteHandler
+	 */
+	public static class Route {
+
+		/**
+		 * The pattern this route uses
+		 */
+		public String pattern;
+
+		/**
+		 * The routeHandler that will handle requests passed to this route
+		 */
+		public RouteHandler routeHandler;
+	}
+
+	public static abstract class RouteHandler {
+		public abstract NGActionResults handle( WrappedURL url, NGContext context );
+	}
+
+	/**
+	 * For returning 404
+	 */
+	public static class NotFoundRouteHandler extends RouteHandler {
+		@Override
+		public NGActionResults handle( final WrappedURL url, NGContext context ) {
+			final NGResponse response = new NGResponse();
+			response.setStatus( 404 );
+			response.setContentString( "Not found: " + url );
+			return response;
+		}
+	}
+
+	public static class BiFunctionRouteHandler extends RouteHandler {
+		private BiFunction<WrappedURL, NGContext, NGActionResults> _biFunction;
+
+		public BiFunctionRouteHandler( final BiFunction<WrappedURL, NGContext, NGActionResults> biFunction ) {
+			_biFunction = biFunction;
+		}
+
+		@Override
+		public NGActionResults handle( WrappedURL url, NGContext context ) {
+			return _biFunction.apply( url, context );
+		}
+	}
+
+	public static class ComponentRouteHandler extends RouteHandler {
+		private Class<? extends NGComponent> _componentClass;
+
+		public ComponentRouteHandler( final Class<? extends NGComponent> componentClass ) {
+			_componentClass = componentClass;
+		}
+
+		@Override
+		public NGActionResults handle( WrappedURL url, NGContext context ) {
+			return NGApplication.application().pageWithName( _componentClass, context );
+		}
+	}
+}
---
+++ ng-appserver/src/main/java/ng/appserver/experimental/WrappedURL.java
@@ -0,0 +1,119 @@
+package ng.appserver.experimental;
+
+/**
+ * Wraps URL paths for easy access to its components.
+ *
+ * - only stores the path (no hostname, no protocol) - does not differentiate between relative and absolute urls
+ */
+
+public class WrappedURL {
+
+	/**
+	 * The URL string this object was generated from.
+	 */
+	private String _sourceURL;
+
+	/**
+	 * The cleaned up URL
+	 */
+	private String _parsedURL;
+
+	/**
+	 * cached copy of the url's elements
+	 */
+	private String[] _pathElements;
+
+	/**
+	 * Instances are constructed using the create() method.
+	 */
+	private WrappedURL() {
+	}
+
+	public static WrappedURL create( final String sourceURL ) {
+		String parsedURL = sourceURL;
+
+		if( parsedURL == null ) {
+			parsedURL = "";
+		}
+
+		if( parsedURL.startsWith( "/" ) ) {
+			parsedURL = parsedURL.substring( 1 );
+		}
+
+		if( parsedURL.endsWith( "/" ) ) {
+			parsedURL = parsedURL.substring( 0, parsedURL.length() - 1 );
+		}
+
+		WrappedURL object = new WrappedURL();
+		object._parsedURL = parsedURL;
+		object._sourceURL = sourceURL;
+		return object;
+	}
+
+	public String sourceURL() {
+		return _sourceURL;
+	}
+
+	@Override
+	public String toString() {
+		return _parsedURL;
+	}
+
+	private String[] pathElements() {
+		if( _pathElements == null ) {
+			_pathElements = _parsedURL.split( "/" );
+		}
+
+		return _pathElements;
+	}
+
+	/**
+	 * @return The string value at [index] in the path. defaultValue if [index] does not exist.
+	 */
+	public String getString( int index, String defaultValue ) {
+
+		if( index > pathElements().length - 1 ) {
+			return defaultValue;
+		}
+
+		return pathElements()[index];
+	}
+
+	/**
+	 * @return The string value at [index] in the path. null if [index] does not exist.
+	 */
+	public String getString( int index ) {
+		return getString( index, null );
+	}
+
+	/**
+	 * @return The integer value at [index] in the path. defaultValue if [index] does not exist.
+	 */
+	public Integer getInteger( int index, Integer defaultValue ) {
+		String value = getString( index );
+
+		if( value == null ) {
+			return defaultValue;
+		}
+
+		return Integer.valueOf( value );
+	}
+
+	/**
+	 * @return The integer value at [index] in the path. null if [index] does not exist.
+	 */
+	public Integer getInteger( int index ) {
+		return getInteger( index, null );
+	}
+
+	/**
+	 * @return The number of elements in the URL
+	 */
+	public int length() {
+		return pathElements().length;
+	}
+
+	public String getNamedParameter( final String parameterName ) {
+		throw new RuntimeException( "Implement" ); // FIXME: Implement
+	}
+}
