d8282816ef27102a99119b81c5812f64047f6922
Hugi Thordarson <hugi@karlmenn.is>
2021-11-28 10:47:45 +0000

    More KVC Benchmarking

--- ng-core/src/main/java/ng/kvc/benchmarks/KVCBenchmark.java
+++ ng-core/src/main/java/ng/kvc/benchmarks/KVCBenchmark.java
@@ -5,25 +5,25 @@ import ng.kvc.NGKeyValueCoding;
 public class KVCBenchmark {
 
 	public static void main( String[] argv ) {
-		Person person = new Person();
-		person._name = "Hugi";
+		run();
+		run();
+		run();
+	}
+
+	public static void run() {
+		Person person = new Person( "Hugi" );
 
 		int numberOfRuns = 10_000_000;
 
 		long start = System.currentTimeMillis();
+
 		for( int i = 0; i < numberOfRuns; i++ ) {
 			Object valueForKey = NGKeyValueCoding.Utility.valueForKey( person, "name" );
-			int inti = ((String)valueForKey).length();
+			int length = ((String)valueForKey).length();
 		}
 
 		System.out.println( "Time: " + (System.currentTimeMillis() - start) );
 	}
 
-	public static class Person {
-		public String _name;
-
-		public String name() {
-			return _name;
-		}
-	}
+	public record Person( String name ) {}
 }
