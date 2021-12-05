be6e9eb5bf131c5a7e46c6da98869cd5e8de7d87
Hugi Thordarson <hugi@karlmenn.is>
2021-12-05 10:18:05 +0000

    Moved the servlet to the adaptor class

--- ng-adaptor-jetty-http2/src/main/java/ng/adaptor/jetty/http2/NGAdaptorJettyHTTP2.java
+++ ng-adaptor-jetty-http2/src/main/java/ng/adaptor/jetty/http2/NGAdaptorJettyHTTP2.java
@@ -1,18 +1,10 @@
 package ng.adaptor.jetty.http2;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.BindException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
@@ -24,16 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.servlet.DispatcherType;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.PushBuilder;
 import ng.appserver.NGAdaptor;
 import ng.appserver.NGApplication;
-import ng.appserver.NGCookie;
-import ng.appserver.NGRequest;
-import ng.appserver.NGResponse;
 
 public class NGAdaptorJettyHTTP2 extends NGAdaptor {
 
@@ -58,8 +46,7 @@ public class NGAdaptorJettyHTTP2 extends NGAdaptor {
 		final ServletHandler servletHandler = new ServletHandler();
 		server.setHandler( servletHandler );
 
-		//		servletHandler.addServletWithMapping( NGServlet.class, "/" );
-		servletHandler.addServletWithMapping( ExperimentalNGTestHTTP2Servlet.class, "/" ); // FIXME: Just checking out HTTP capabilities
+		servletHandler.addServletWithMapping( NGServlet.class, "/" );
 		servletHandler.addFilterWithMapping( PushCacheFilter.class, "/", EnumSet.of( DispatcherType.REQUEST ) );
 
 		try {
@@ -68,7 +55,6 @@ public class NGAdaptorJettyHTTP2 extends NGAdaptor {
 		catch( final Exception e ) {
 			if( NGApplication.isDevelopmentMode() && e instanceof IOException && e.getCause() instanceof BindException ) {
 				logger.info( "Our port seems to be in use and we're in development mode. Let's try murdering the bastard that's blocking us" );
-				stopPreviousDevelopmentInstance( port );
 				start();
 			}
 			else {
@@ -84,93 +70,26 @@ public class NGAdaptorJettyHTTP2 extends NGAdaptor {
 	}
 
 	public static class NGServlet extends HttpServlet {
-
 		@Override
-		protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-			doRequest( request, response );
-		}
+		protected void doGet( HttpServletRequest req, HttpServletResponse resp ) {
 
-		@Override
-		protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-			doRequest( request, response );
-		}
+			PushBuilder pushBuilder = req.newPushBuilder();
+			//		PushBuilder pushBuilder = Request.getBaseRequest( req ).newPushBuilder();
 
-		private void doRequest( final HttpServletRequest servletRequest, final HttpServletResponse servletResponse ) throws ServletException, IOException {
+			pushBuilder
+					.path( "images/kodedu-logo.png" )
+					.addHeader( "content-type", "image/png" )
+					.push();
 
-			// This is where the application logic will perform it's actual work
-			final NGRequest woRequest = servletRequestToNGRequest( servletRequest );
-			final NGResponse ngResponse = NGApplication.application().dispatchRequest( woRequest );
-
-			servletResponse.setStatus( ngResponse.status() );
-
-			servletResponse.setHeader( "content-length", String.valueOf( ngResponse.contentBytes().length ) );
-
-			for( final Entry<String, List<String>> entry : ngResponse.headers().entrySet() ) {
-				for( final String headerValue : entry.getValue() ) {
-					servletResponse.addHeader( entry.getKey(), headerValue );
-				}
+			try( PrintWriter respWriter = resp.getWriter() ;) {
+				respWriter.write( "<html>" +
+						"<img src='images/kodedu-logo.png'>" +
+						"</html>" );
 			}
-
-			try( final OutputStream out = servletResponse.getOutputStream()) {
-				out.write( ngResponse.contentBytes() );
+			catch( IOException e ) {
+				throw new RuntimeException( e );
 			}
 		}
-	}
-
-	/**
-	 * @return the given HttpServletRequest converted to an NGRequest
-	 *
-	 * FIXME: We're not passing in the request parameters
-	 * FIXME: WE need to read the request's content as well
-	 */
-	private static NGRequest servletRequestToNGRequest( final HttpServletRequest sr ) {
-		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-		try( final InputStream is = sr.getInputStream()) {
-			is.transferTo( bos );
-		}
-		catch( final IOException e ) {
-			throw new RuntimeException( e ); // FIXME: We're throwing RuntimeExceptions all over the place, we should probably be using NGForwardException, once that's structured
-		}
-
-		final NGRequest request = new NGRequest( sr.getMethod(), sr.getRequestURI(), sr.getProtocol(), headerMap( sr ), bos.toByteArray() );
-
-		final Cookie[] cookies = sr.getCookies();
-
-		if( cookies != null ) {
-			for( Cookie cookie : cookies ) {
-				request.addCookie( servletCookieToNGCookie( cookie ) );
-			}
-		}
-
-		return request;
-	}
-
-	private static NGCookie servletCookieToNGCookie( Cookie sc ) {
-		return new NGCookie( sc.getName(), sc.getValue(), sc.getDomain(), sc.getPath(), sc.getSecure(), sc.getMaxAge() );
-	}
-
-	/**
-	 * FIXME: Implement
-	 */
-	private static Map<String, List<String>> headerMap( final HttpServletRequest sr ) {
-		final Map<String, List<String>> map = new HashMap<>();
-
-		final Enumeration<String> headerNamesEnumeration = sr.getHeaderNames();
-
-		while( headerNamesEnumeration.hasMoreElements() ) {
-			final String headerName = headerNamesEnumeration.nextElement();
-			final List<String> values = new ArrayList<>();
-			map.put( headerName, values );
-
-			final Enumeration<String> headerValuesEnumeration = sr.getHeaders( headerName );
-
-			while( headerValuesEnumeration.hasMoreElements() ) {
-				values.add( headerValuesEnumeration.nextElement() );
-			}
-		}
-
-		return map;
 	}
 
 	/**
