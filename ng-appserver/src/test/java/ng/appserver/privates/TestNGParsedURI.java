de16ee085922d19c5d21fb1eff8813a932f4cc37
Hugi Thordarson <hugi@karlmenn.is>
2021-12-29 12:54:08 +0000

    Add a few more tests for NGParsedURI

--- ng-appserver/src/test/java/ng/appserver/privates/TestNGParsedURI.java
+++ ng-appserver/src/test/java/ng/appserver/privates/TestNGParsedURI.java
@@ -1,5 +1,6 @@
 package ng.appserver.privates;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -8,9 +9,17 @@ import org.junit.jupiter.api.Test;
 public class TestNGParsedURI {
 
 	@Test
-	public void testOf() {
+	public void testOfEmptyURI() {
 		NGParsedURI parsedURI = NGParsedURI.of( "/" );
 		assertTrue( parsedURI.elementAt( 0 ).isEmpty() );
+	}
+
+	@Test
+	public void testOf() {
+		NGParsedURI parsedURI = NGParsedURI.of( "/smu/bla/" );
+		assertTrue( parsedURI.getString( 0 ).equals( "smu" ) );
+		assertTrue( parsedURI.getString( 1 ).equals( "bla" ) );
+		assertNull( parsedURI.getString( 2 ) );
 	}
 
 	@Test
