304a2cb053ee33c6094269ec0c8fc5ec1665488e
Hugi Thordarson <hugi@karlmenn.is>
2021-12-29 12:21:26 +0000

    Whoops, fix switched up lifebeatEnabled property

--- ng-appserver/src/main/java/ng/appserver/NGApplication.java
+++ ng-appserver/src/main/java/ng/appserver/NGApplication.java
@@ -77,7 +77,7 @@ public class NGApplication {
 			System.exit( -1 );
 		}
 
-		if( !_properties.propWOLifebeatEnabled() ) {
+		if( _properties.propWOLifebeatEnabled() ) {
 			startLifebeatThread();
 		}
 
