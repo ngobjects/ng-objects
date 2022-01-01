ca79b764c1cd506973f54b3a83bebc3a7736f6f6
Hugi Thordarson <hugi@karlmenn.is>
2022-01-01 17:38:30 +0000

    Convert NGCookie from a record to a class

--- ng-appserver/src/main/java/ng/appserver/NGCookie.java
+++ ng-appserver/src/main/java/ng/appserver/NGCookie.java
@@ -7,11 +7,83 @@ package ng.appserver;
 // FIXME: We need to decide how to handle timeouts.
 // FIXME: We need to decide how to set-cookie. And even if that should be a separate class, since requests will only contain key-value pairs
 
-public record NGCookie(
-		String name,
-		String value,
-		String domain,
-		String path,
-		boolean isSecure,
-		int maxAge ) {
+public class NGCookie {
+
+	private String _name;
+	private String _value;
+	private String _domain;
+	private String _path;
+	private String _comment;
+	private int _maxAge;
+	private boolean _isSecure;
+	private boolean _isHttpOnly;
+
+	public NGCookie( final String name, final String value ) {
+		this.setName( name );
+		this.setValue( value );
+	}
+
+	public boolean isHttpOnly() {
+		return _isHttpOnly;
+	}
+
+	public void setHttpOnly( boolean isHttpOnly ) {
+		_isHttpOnly = isHttpOnly;
+	}
+
+	public boolean isSecure() {
+		return _isSecure;
+	}
+
+	public void setSecure( boolean isSecure ) {
+		_isSecure = isSecure;
+	}
+
+	public int maxAge() {
+		return _maxAge;
+	}
+
+	public void setMaxAge( int maxAge ) {
+		_maxAge = maxAge;
+	}
+
+	public String comment() {
+		return _comment;
+	}
+
+	public void setComment( String comment ) {
+		_comment = comment;
+	}
+
+	public String path() {
+		return _path;
+	}
+
+	public void setPath( String path ) {
+		_path = path;
+	}
+
+	public String domain() {
+		return _domain;
+	}
+
+	public void setDomain( String domain ) {
+		_domain = domain;
+	}
+
+	public String value() {
+		return _value;
+	}
+
+	public void setValue( String value ) {
+		_value = value;
+	}
+
+	public String name() {
+		return _name;
+	}
+
+	public void setName( String name ) {
+		_name = name;
+	}
 }
