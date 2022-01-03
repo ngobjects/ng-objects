0594f681d3a624e2c57487183c2b46a92396f892
Hugi Thordarson <hugi@karlmenn.is>
2022-01-03 13:36:08 +0000

    Add test cases for setting and appending header values

--- ng-appserver/src/test/java/ng/appserver/privates/TestNGMessage.java
+++ ng-appserver/src/test/java/ng/appserver/privates/TestNGMessage.java
@@ -11,10 +11,25 @@ import ng.appserver.NGResponse;
 public class TestNGMessage {
 
 	@Test
-	public void testHeadersDictionaryIsCaseInsensitive() {
+	public void headersDictionaryIsCaseInsensitive() {
 		NGResponse r = new NGResponse();
 		r.setHeader( "some-header", "some-value" );
+		assertEquals( List.of( "some-value" ), r.headers().get( "SoMe-HeADeR" ) );
+	}
 
-		assertEquals( List.of( "some-value" ), r.headers().get( "Some-Header" ) );
+	@Test
+	public void setHeaderReplacesExistingHeaderValues() {
+		NGResponse r = new NGResponse();
+		r.setHeader( "some-header", "some-value" );
+		r.setHeader( "some-header", "some-other-value" );
+		assertEquals( List.of( "some-other-value" ), r.headers().get( "some-header" ) );
+	}
+
+	@Test
+	public void appendHeaderKeepsExistingHeaderValues() {
+		NGResponse r = new NGResponse();
+		r.appendHeader( "some-header", "some-value" );
+		r.appendHeader( "some-header", "some-other-value" );
+		assertEquals( List.of( "some-value", "some-other-value" ), r.headers().get( "some-header" ) );
 	}
 }
