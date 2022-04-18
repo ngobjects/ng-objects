908518a3da574726c94f0857c5a52341e0b40c5c
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 17:38:14 +0000

    Add a very, very basic initial implementation of NGHyperlink

--- ng-appserver/src/main/java/ng/appserver/elements/NGHyperlink.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGHyperlink.java
@@ -3,11 +3,25 @@ package ng.appserver.elements;
 import java.util.Map;
 
 import ng.appserver.NGAssociation;
+import ng.appserver.NGContext;
 import ng.appserver.NGElement;
+import ng.appserver.NGResponse;
 
 public class NGHyperlink extends NGDynamicGroup {
 
+	private final NGAssociation _hrefAssociation;
+
 	public NGHyperlink( String name, Map<String, NGAssociation> associations, NGElement template ) {
 		super( name, associations, template );
+		_hrefAssociation = associations.get( "href" );
+	}
+
+	@Override
+	public void appendToResponse( NGResponse response, NGContext context ) {
+		final String href = (String)_hrefAssociation.valueInComponent( context.component() );
+
+		response.appendContentString( "<a href=\"" + href + "\">" );
+		appendChildrenToResponse( response, context );
+		response.appendContentString( "</a>" );
 	}
 }
