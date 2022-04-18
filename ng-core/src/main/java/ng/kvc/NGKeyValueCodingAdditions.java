284148cc2e0f76b3689b9ea9ee419847da13e336
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 14:23:14 +0000

    Very rudimentary implementation of NGKeyValueCodingAdditions

--- ng-core/src/main/java/ng/kvc/NGKeyValueCodingAdditions.java
+++ ng-core/src/main/java/ng/kvc/NGKeyValueCodingAdditions.java
@@ -1,8 +1,50 @@
 package ng.kvc;
 
-public interface NGKeyValueCodingAdditions {
+import java.util.Objects;
 
-	public Object valueForKeyPath( String keyPath );
+public interface NGKeyValueCodingAdditions extends NGKeyValueCoding {
 
-	public void takeValueForKeyPath( Object value, String keyPath );
+	public Object valueForKeyPath( final String keyPath );
+
+	public void takeValueForKeyPath( final Object value, final String keyPath );
+
+	public static class Utility {
+
+		public static Object valueForKeyPath( final Object object, final String key ) {
+			Objects.requireNonNull( object );
+			Objects.requireNonNull( key );
+
+			if( object instanceof NGKeyValueCodingAdditions kvcAdditionsObject ) {
+				return kvcAdditionsObject.valueForKeyPath( key );
+			}
+
+			return DefaultImplementation.valueForKeyPath( object, key );
+		}
+	}
+
+	public static class DefaultImplementation {
+
+		/**
+		 * Temporary implementation for testing
+		 */
+		public static Object valueForKeyPath( final Object object, final String keyPath ) {
+			String[] keyPaths = keyPath.split( "\\." );
+
+			Object result = object;
+
+			for( String currentKeyPath : keyPaths ) {
+				result = NGKeyValueCoding.Utility.valueForKey( result, currentKeyPath );
+
+				if( result == null ) {
+					return null;
+				}
+			}
+
+			return result;
+		}
+
+		public void takeValueForKeyPath( Object object, Object target, String keyPath ) {
+			throw new RuntimeException( "Not implemented" );
+		}
+	}
 }
