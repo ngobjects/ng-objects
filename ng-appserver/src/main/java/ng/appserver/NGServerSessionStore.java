d0f3bdca68f732c14ef043f116044bf7d840b3df
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 11:17:29 +0000

    Tiny little cleanup in the session store

--- ng-appserver/src/main/java/ng/appserver/NGServerSessionStore.java
+++ ng-appserver/src/main/java/ng/appserver/NGServerSessionStore.java
@@ -4,13 +4,16 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Default sessionstore
+ * Default session store
+ *
+ * FIXME: Implement session timeouts
  */
 
 public class NGServerSessionStore extends NGSessionStore {
 
-	final Map<String,NGSession> _sessions = new ConcurrentHashMap<>();
-	
+	final Map<String, NGSession> _sessions = new ConcurrentHashMap<>();
+
+	@Override
 	public NGSession checkoutSessionWithID( final String sessionID ) {
 		return _sessions.get( sessionID );
 	}
--- ng-appserver/src/main/java/ng/appserver/NGSessionStore.java
+++ ng-appserver/src/main/java/ng/appserver/NGSessionStore.java
@@ -1,18 +1,6 @@
 package ng.appserver;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * FIXME: This should be an abstract class or an interface. We want persistent sessions at some point.
- * FIXME: Actually implement  
- */
-
 public abstract class NGSessionStore {
 
-	Map<String,NGSession> _sessions = new HashMap<>();
-	
-	public NGSession checkoutSessionWithID( final String sessionID ) {
-		return _sessions.get( sessionID );
-	}
+	public abstract NGSession checkoutSessionWithID( final String sessionID );
 }
