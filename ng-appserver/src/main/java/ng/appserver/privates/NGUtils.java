f51596b6a9a382595ff5d6a3467cb366b50a3cd1
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 11:26:25 +0000

    Utility methods for getting resource bytes in a centralized way

--- ng-appserver/src/main/java/ng/appserver/privates/NGUtils.java
+++ ng-appserver/src/main/java/ng/appserver/privates/NGUtils.java
@@ -2,6 +2,7 @@ package ng.appserver.privates;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -13,11 +14,17 @@ public class NGUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger( NGUtils.class );
 
+	private static final String APP_RESOURCES_FOLDER = "app-resources";
+	private static final String COMPONENTS_FOLDER = "components";
+
 	/**
 	 * Reads the content of the given Java resource
 	 */
 	public static Optional<byte[]> readJavaResource( final String resourcePath ) {
+		Objects.requireNonNull( resourcePath );
+
 		logger.info( "Reading resource from path: " + resourcePath );
+
 		try( final InputStream resourceAsStream = NGResourceManager.class.getResourceAsStream( resourcePath )) {
 
 			if( resourceAsStream == null ) {
@@ -32,6 +39,19 @@ public class NGUtils {
 	}
 
 	public static String resourcePath( final String folderName, final String resourcePath ) {
+		Objects.requireNonNull( folderName );
+		Objects.requireNonNull( resourcePath );
+
 		return "/" + folderName + "/" + resourcePath;
+	}
+
+	public static Optional<byte[]> bytesForAppResource( final String resourcePath ) {
+		Objects.requireNonNull( resourcePath );
+		return readJavaResource( resourcePath( APP_RESOURCES_FOLDER, resourcePath ) );
+	}
+
+	public static Optional<byte[]> bytesForComponentResource( final String resourcePath ) {
+		Objects.requireNonNull( resourcePath );
+		return readJavaResource( resourcePath( COMPONENTS_FOLDER, resourcePath ) );
 	}
 }
