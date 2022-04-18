caa2da26c35d577e4d89922d93a4b2118cdd21b5
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 14:40:57 +0000

    Add more testing for programmatic component generation

--- ng-testapp/src/main/java/ng/testapp/components/ProgrammaticDynamicComponent.java
+++ ng-testapp/src/main/java/ng/testapp/components/ProgrammaticDynamicComponent.java
@@ -46,28 +46,33 @@ public class ProgrammaticDynamicComponent extends NGComponent {
 		g._children.add( new NGHTMLBareString( "<html>\n" ) );
 		g._children.add( new NGHTMLBareString( "<head>\n" ) );
 
-		final Map<String, NGAssociation> style = new HashMap<>();
-		style.put( "filename", new NGConstantValueAssociation( "main.css" ) );
-		g._children.add( new NGStylesheet( "wat?", style, null ) );
+		final Map<String, NGAssociation> stylesheet = new HashMap<>();
+		stylesheet.put( "filename", new NGConstantValueAssociation( "main.css" ) );
+		g._children.add( new NGStylesheet( "wat?", stylesheet, null ) );
 
 		g._children.add( new NGHTMLBareString( "</head>\n" ) );
 		g._children.add( new NGHTMLBareString( "<body>\n" ) );
-		g._children.add( new NGHTMLBareString( "<p>Hello</p>\n" ) );
 
-		final Map<String, NGAssociation> m1 = new HashMap<>();
-		m1.put( "filename", new NGConstantValueAssociation( "test-image-1.jpg" ) );
-		m1.put( "width", new NGConstantValueAssociation( 200 ) );
-		g._children.add( new NGImage( "wat?", m1, null ) );
+		g._children.add( new NGHTMLBareString( "<h2>This is a programmatically generated component template</h2>\n" ) );
 
-		final Map<String, NGAssociation> m2 = new HashMap<>();
-		m2.put( "filename", new NGKeyValueAssociation( "someMethodReturningImageFilename" ) );
-		m2.put( "width", new NGConstantValueAssociation( 200 ) );
-		g._children.add( new NGImage( "wat?", m2, null ) );
+		final Map<String, NGAssociation> image1 = new HashMap<>();
+		image1.put( "filename", new NGConstantValueAssociation( "test-image-1.jpg" ) );
+		image1.put( "width", new NGConstantValueAssociation( 200 ) );
+		g._children.add( new NGImage( "wat?", image1, null ) );
 
-		final Map<String, NGAssociation> m3 = new HashMap<>();
-		m3.put( "filename", new NGKeyValueAssociation( "someMethodReturningFilenameHolder.filename" ) );
-		m3.put( "width", new NGConstantValueAssociation( 200 ) );
-		g._children.add( new NGImage( "wat?", m3, null ) );
+		g._children.add( new NGHTMLBareString( "<br><br>" ) );
+
+		final Map<String, NGAssociation> image2 = new HashMap<>();
+		image2.put( "filename", new NGKeyValueAssociation( "someMethodReturningImageFilename" ) );
+		image2.put( "width", new NGConstantValueAssociation( 200 ) );
+		g._children.add( new NGImage( "wat?", image2, null ) );
+
+		g._children.add( new NGHTMLBareString( "<br><br>" ) );
+
+		final Map<String, NGAssociation> image3 = new HashMap<>();
+		image3.put( "filename", new NGKeyValueAssociation( "someMethodReturningFilenameHolder.filename" ) );
+		image3.put( "width", new NGConstantValueAssociation( 200 ) );
+		g._children.add( new NGImage( "wat?", image3, null ) );
 
 		g._children.add( new NGHTMLBareString( "</body>\n" ) );
 		g._children.add( new NGHTMLBareString( "</html>\n" ) );
