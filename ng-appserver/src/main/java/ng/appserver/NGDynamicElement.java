b61c5ab738017fb3ad5ca9286ef654c37e80cf81
Hugi Thordarson <hugi@karlmenn.is>
2022-04-18 14:36:41 +0000

    Add a comment on NGDynamicElements name parameter

--- ng-appserver/src/main/java/ng/appserver/NGDynamicElement.java
+++ ng-appserver/src/main/java/ng/appserver/NGDynamicElement.java
@@ -4,5 +4,8 @@ import java.util.Map;
 
 public abstract class NGDynamicElement extends NGElement {
 
+	/**
+	 * FIXME: Not sure what to do with the [name] parameter, only added it since it's a part of the WO APIs // Hugi 2022-04-18
+	 */
 	public NGDynamicElement( String name, Map<String, NGAssociation> associations, NGElement template ) {}
 }
