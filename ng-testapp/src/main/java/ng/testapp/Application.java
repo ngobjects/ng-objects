b6a40c2940896472b11603e6f6f7b5397379b070
Hugi Thordarson <hugi@karlmenn.is>
2022-04-19 11:00:04 +0000

    Add more example routes

--- ng-testapp/src/main/java/ng/testapp/Application.java
+++ ng-testapp/src/main/java/ng/testapp/Application.java
@@ -7,6 +7,9 @@ import ng.appserver.NGApplication;
 import ng.appserver.NGCookie;
 import ng.appserver.NGRequest;
 import ng.appserver.NGResponse;
+import ng.appserver.privates.NGUtils;
+import ng.testapp.components.ExampleComponent;
+import ng.testapp.components.ProgrammaticDynamicComponent;
 
 public class Application extends NGApplication {
 
@@ -17,9 +20,40 @@ public class Application extends NGApplication {
 	}
 
 	public Application() {
-		routeTable().map( "/some-route", ( request ) -> {
+		routeTable().map( "/response-plain", ( request ) -> {
 			NGResponse response = new NGResponse( "Oh look, a response!", 200 );
 			response.addCookie( new NGCookie( "nafn", "Hugi" ) );
+			return response;
+		} );
+
+		routeTable().map( "/component-programmatic", ( request ) -> {
+			return pageWithName( ProgrammaticDynamicComponent.class, request.context() );
+		} );
+
+		routeTable().map( "/component-plain", ( request ) -> {
+			return pageWithName( ExampleComponent.class, request.context() );
+		} );
+
+		routeTable().map( "/image", ( request ) -> {
+			NGResponse response = new NGResponse();
+			response.setContentBytes( NGUtils.readWebserverResource( "test-image-4.jpg" ).get() );
+			response.setHeader( "content-type", "image/jpeg" );
+			return response;
+		} );
+
+		routeTable().map( "/print-routes", ( request ) -> {
+			StringBuilder b = new StringBuilder();
+
+			routeTable().routes().forEach( route -> {
+				b.append( route.pattern );
+				b.append( " -> " );
+				b.append( route.routeHandler.getClass().getName() );
+				b.append( "\n" );
+			} );
+
+			NGResponse response = new NGResponse();
+			response.setContentString( b.toString() );
+			response.setHeader( "content-type", "text/plain" );
 			return response;
 		} );
 	}
