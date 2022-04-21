0037a3b6531292ce814912578a14ff99c9c945e1
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 17:12:44 +0000

    Syntax cleanup in NS-classes

--- ng-appserver/src/main/java/ng/appserver/templating/_NSDictionary.java
+++ ng-appserver/src/main/java/ng/appserver/templating/_NSDictionary.java
@@ -6,12 +6,11 @@ import java.util.HashMap;
 
 public class _NSDictionary<K, V> extends HashMap<K, V> {
 
+	public _NSDictionary() {}
+
 	public _NSDictionary( V value, K key ) {
-		super();
 		put( key, value );
 	}
-
-	public _NSDictionary() {}
 
 	public Enumeration objectEnumerator() {
 		return Collections.enumeration( this.values() );
