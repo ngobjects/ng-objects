08a509a9c168cdc0a0c59e8379849ec6c99cacc9
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 11:12:28 +0000

    A bit nicer class location thingy

--- ng-appserver/src/main/java/er/extensions/bettertemplates/_NGUtilities.java
+++ ng-appserver/src/main/java/er/extensions/bettertemplates/_NGUtilities.java
@@ -1,23 +1,27 @@
 package er.extensions.bettertemplates;
 
 import java.lang.reflect.Constructor;
+import java.util.List;
 
+import ng.appserver.elements.NGHyperlink;
 import ng.appserver.elements.NGImage;
 import ng.appserver.elements.NGString;
 
 public class _NGUtilities {
 
-	public static Class classWithName( String s1 ) {
+	private static List<Class> classes() {
+		return List.of( NGString.class, NGImage.class, NGHyperlink.class );
+	}
 
-		if( s1.contains( "NGString" ) ) {
-			return NGString.class;
+	public static Class classWithName( String className ) {
+
+		for( Class c : classes() ) {
+			if( c.getName().contains( className ) ) {
+				return c;
+			}
 		}
 
-		if( s1.contains( "NGImage" ) ) {
-			return NGImage.class;
-		}
-
-		throw new RuntimeException( "Not implemented" );
+		throw new RuntimeException( "Clas not found: " + className );
 	}
 
 	public static Object instantiateObject( Class objectClass, Class[] parameterTypes, Object[] parameters, boolean shouldThrow, boolean shouldLog ) {
