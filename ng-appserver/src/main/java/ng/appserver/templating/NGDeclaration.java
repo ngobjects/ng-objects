3a941143a02b5a807ee35bd0893f756867314640
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 12:06:53 +0000

    Syntax cleanup for NGDeclaration

--- ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
@@ -7,22 +7,44 @@ import ng.appserver.NGAssociation;
 import ng.appserver.NGComponent;
 
 public class NGDeclaration {
-	String _name;
-	String _type;
-	_NSMutableDictionary<String, NGAssociation> _associations;
+
+	final String _name;
+	final String _type;
+	final _NSMutableDictionary<String, NGAssociation> _associations;
 
 	public NGDeclaration( String aName, String aType, _NSDictionary<String, NGAssociation> theAssocations ) {
-		this._name = aName;
-		this._type = aType;
-		this._associations = theAssocations.mutableClone();
-		this._setDebuggingForAssociations();
+		_name = aName;
+		_type = aType;
+		_associations = theAssocations.mutableClone();
+		_setDebuggingForAssociations();
+	}
+
+	public String name() {
+		return _name;
+	}
+
+	public String type() {
+		return _type;
+	}
+
+	public _NSDictionary<String, NGAssociation> associations() {
+		return _associations;
+	}
+
+	@Override
+	public String toString() {
+		return "<" + getClass().getName() + " name = " + _name + " type = " + _type + " associations " + _associations.toString() + " >";
+	}
+
+	public String stringRepresentation() {
+		return _name + ":" + _type + " " + associations().toString() + "\n";
 	}
 
 	private void _setDebuggingForAssociations() {
-		NGAssociation aDebugAssoc = this._associations.remove( "WODebug" );
+		NGAssociation aDebugAssoc = _associations.remove( "WODebug" );
 		if( aDebugAssoc != null ) {
 			String aBindingName = null;
-			Enumeration aKeyEnumerator = this._associations.keyEnumerator();
+			Enumeration aKeyEnumerator = _associations.keyEnumerator();
 
 			while( true ) {
 				NGAssociation anAssociation;
@@ -32,35 +54,13 @@ public class NGDeclaration {
 					}
 
 					aBindingName = (String)aKeyEnumerator.nextElement();
-					anAssociation = (NGAssociation)this._associations.objectForKey( aBindingName );
-					anAssociation.setDebugEnabledForBinding( aBindingName, this._name, this._type );
+					anAssociation = (NGAssociation)_associations.objectForKey( aBindingName );
+					anAssociation.setDebugEnabledForBinding( aBindingName, _name, _type );
 				}
 				while( aDebugAssoc.isValueConstant() && aDebugAssoc.valueInComponent( (NGComponent)null ) != null && NGApplication.application().isDebuggingEnabled() );
 
 				anAssociation._setDebuggingEnabled( false );
 			}
 		}
-	}
-
-	public String name() {
-		return this._name;
-	}
-
-	public String type() {
-		return this._type;
-	}
-
-	public _NSDictionary<String, NGAssociation> associations() {
-		return this._associations;
-	}
-
-	@Override
-	public String toString() {
-		return "<" + this.getClass().getName() + " name = " + this._name + " type = " + this._type + " associations "
-				+ this._associations.toString() + " >";
-	}
-
-	public String stringRepresentation() {
-		return this._name + ":" + this._type + " " + this.associations().toString() + "\n";
 	}
 }
