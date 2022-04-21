c9814e5df936458e19171e55e39cf49ece47114e
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 10:53:16 +0000

    Horrible _NGUtilities additions

--- ng-appserver/src/main/java/er/extensions/bettertemplates/_NGUtilities.java
+++ ng-appserver/src/main/java/er/extensions/bettertemplates/_NGUtilities.java
@@ -1,8 +1,33 @@
 package er.extensions.bettertemplates;
 
+import java.lang.reflect.Constructor;
+
+import ng.appserver.elements.NGString;
+
 public class _NGUtilities {
 
 	public static Class classWithName( String s1 ) {
+		if( s1.contains( "NGString" ) ) {
+			return NGString.class;
+		}
+
 		throw new RuntimeException( "Not implemented" );
+	}
+
+	public static Object instantiateObject( Class objectClass, Class[] parameterTypes, Object[] parameters, boolean shouldThrow, boolean shouldLog ) {
+		try {
+			if( (parameterTypes != null) && (parameters != null) ) {
+				Constructor constructor = objectClass.getDeclaredConstructor( parameterTypes );
+				return constructor.newInstance( parameters );
+			}
+			else {
+				return objectClass.newInstance();
+			}
+		}
+		catch( Throwable throwable ) {
+
+			throwable.printStackTrace();
+		}
+		return null;
 	}
 }
