8a799b50fd3458683cb07d4d6d56f3f8d4e2ff24
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 12:01:48 +0000

    Deleted NGHelperFunctionConstantValueAssociation

--- ng-appserver/src/main/java/ng/appserver/templating/NGHelperFunctionAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGHelperFunctionAssociation.java
@@ -1,6 +1,7 @@
 package ng.appserver.templating;
 
 import ng.appserver.NGAssociation;
+import ng.appserver.NGConstantValueAssociation;
 
 /**
  * Factory methods for creating WOAssociations (ripped from WOAssociation)
@@ -8,28 +9,18 @@ import ng.appserver.NGAssociation;
  * @author mschrag
  */
 public class NGHelperFunctionAssociation {
+
 	private static boolean _keyPathIsReadOnly( String keyPath ) {
 		return keyPath.startsWith( "@" ) || keyPath.indexOf( ".@" ) > 0;
 	}
 
-	/**
-	 * Returns a WOAssociation with a value.
-	 *
-	 * @param obj the value
-	 * @return a corresponding WOAssociation
-	 */
 	public static NGAssociation associationWithValue( Object obj ) {
-		return new NGHelperFunctionConstantValueAssociation( obj );
+		return new NGConstantValueAssociation( obj );
 	}
 
-	/**
-	 * Creates a WOAssociation with a keyPath.
-	 *
-	 * @param keyPath the keypath
-	 * @return a corresponding WOAssociation
-	 */
 	public static NGAssociation associationWithKeyPath( String keyPath ) {
 		NGAssociation association;
+
 		if( keyPath.charAt( 0 ) == '^' ) {
 			//			association = new NGHelperFunctionBindingNameAssociation( keyPath );
 			throw new RuntimeException( "Binding name associations are not supported" );
@@ -43,6 +34,7 @@ public class NGHelperFunctionAssociation {
 		else {
 			association = new NGHelperFunctionKeyValueAssociation( keyPath );
 		}
+
 		return association;
 	}
 }
