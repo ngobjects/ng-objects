5b2629c7c783c3b8b65794bce204d97f127ffc25
Hugi Thordarson <hugi@karlmenn.is>
2022-04-21 11:56:07 +0000

    Syntax cleanup

--- ng-appserver/src/main/java/ng/appserver/templating/NGHelperFunctionConstantValueAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGHelperFunctionConstantValueAssociation.java
@@ -3,6 +3,7 @@ package ng.appserver.templating;
 import ng.appserver.NGConstantValueAssociation;
 
 public class NGHelperFunctionConstantValueAssociation extends NGConstantValueAssociation {
+
 	public NGHelperFunctionConstantValueAssociation( Object obj ) {
 		super( obj );
 	}
