1430a3aaff82a27d59b2fb189293fa3e9c4c8fac
Hugi Thordarson <hugi@karlmenn.is>
2022-04-22 14:46:05 +0000

    Deleted the old NGComponentTemplateParser

--- ng-appserver/src/main/java/ng/appserver/NGComponentDefinition.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponentDefinition.java
@@ -1,11 +1,14 @@
 package ng.appserver;
 
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
+import ng.appserver.privates.NGUtils;
 import ng.appserver.templating.NGHelperFunctionDeclarationFormatException;
 import ng.appserver.templating.NGHelperFunctionHTMLFormatException;
 import ng.appserver.templating.NGHelperFunctionParser;
@@ -50,8 +53,8 @@ public class NGComponentDefinition {
 	 */
 	public NGElement template() {
 		try {
-			final String htmlTemplateString = NGComponentTemplateParser.loadHTMLTemplateString( _name );
-			final String wodString = NGComponentTemplateParser.loadWODTemplateString( _name );
+			final String htmlTemplateString = loadHTMLTemplateString( _name );
+			final String wodString = loadWODTemplateString( _name );
 			final List<String> languages = Collections.emptyList();
 			return new NGHelperFunctionParser( htmlTemplateString, wodString, languages ).parse();
 		}
@@ -62,5 +65,39 @@ public class NGComponentDefinition {
 
 	public NGComponentReference componentReferenceWithAssociations( Map<String, NGAssociation> associations, NGElement element ) {
 		return new NGComponentReference( _className, associations, element );
+	}
+
+	/**
+	 * @return The HTML template for the named component
+	 */
+	private static String loadHTMLTemplateString( final String templateName ) {
+		Objects.requireNonNull( templateName );
+		return loadTemplateFile( templateName, "html" );
+	}
+
+	/**
+	 * @return The WOD template for the named component
+	 */
+	private static String loadWODTemplateString( final String templateName ) {
+		Objects.requireNonNull( templateName );
+		return loadTemplateFile( templateName, "wod" );
+	}
+
+	/**
+	 * @return The string template for the named component
+	 */
+	private static String loadTemplateFile( final String templateName, final String extension ) {
+		Objects.requireNonNull( templateName );
+		Objects.requireNonNull( extension );
+
+		final String htmlTemplateFilename = templateName + ".wo/" + templateName + "." + extension;
+
+		final Optional<byte[]> templateBytes = NGUtils.readComponentResource( htmlTemplateFilename );
+
+		if( templateBytes.isEmpty() ) {
+			throw new RuntimeException( "Template not found" );
+		}
+
+		return new String( templateBytes.get(), StandardCharsets.UTF_8 );
 	}
 }
