5b9a1439badad5bd9196fc8af9eeffafe114611f
Hugi Thordarson <hugi@karlmenn.is>
2022-04-22 20:00:21 +0000

    Changed NGDeclaration to a record

--- ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
@@ -5,30 +5,7 @@ import java.util.Map;
 import ng.appserver.NGAssociation;
 
 /**
- * Represents a declaration in a .wod file
+ * Represents a declaration of a dynamic tag
  */
 
-public class NGDeclaration {
-
-	private final String _name;
-	private final String _type;
-	private final Map<String, NGAssociation> _associations;
-
-	public NGDeclaration( String aName, String aType, Map<String, NGAssociation> theAssocations ) {
-		_name = aName;
-		_type = aType;
-		_associations = theAssocations;
-	}
-
-	public String name() {
-		return _name;
-	}
-
-	public String type() {
-		return _type;
-	}
-
-	public Map<String, NGAssociation> associations() {
-		return _associations;
-	}
-}
+public record NGDeclaration( String name, String type, Map<String, NGAssociation> associations ) {}
