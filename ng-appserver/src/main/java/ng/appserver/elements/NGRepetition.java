f022ad467949042964f966df43096bdc43a03ff9
Hugi Thordarson <hugi@karlmenn.is>
2022-04-23 12:01:20 +0000

    Added item and list bindings

--- ng-appserver/src/main/java/ng/appserver/elements/NGRepetition.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGRepetition.java
@@ -1,11 +1,16 @@
 package ng.appserver.elements;
 
+import java.util.List;
 import java.util.Map;
 
 import ng.appserver.NGAssociation;
 import ng.appserver.NGContext;
 import ng.appserver.NGElement;
 import ng.appserver.NGResponse;
+
+/**
+ * Missing [index]
+ */
 
 public class NGRepetition extends NGDynamicGroup {
 
@@ -14,17 +19,41 @@ public class NGRepetition extends NGDynamicGroup {
 	 */
 	private final NGAssociation _count;
 
+	/**
+	 * The object that will take on the value from [list] during each iteration
+	 */
+	private final NGAssociation _item;
+
+	/**
+	 * List of objects to iterate over
+	 */
+	private final NGAssociation _list;
+
 	public NGRepetition( String _name, Map<String, NGAssociation> associations, NGElement element ) {
 		super( _name, associations, element );
 		_count = associations.get( "count" );
+		_item = associations.get( "item" );
+		_list = associations.get( "list" );
 	}
 
 	@Override
 	public void appendToResponse( NGResponse response, NGContext context ) {
-		final int count = Integer.parseInt( (String)_count.valueInComponent( context.component() ) );
 
-		for( int i = 0; i < count; ++i ) {
-			appendChildrenToResponse( response, context );
+		if( _count != null ) {
+			final int count = Integer.parseInt( (String)_count.valueInComponent( context.component() ) );
+
+			for( int i = 0; i < count; ++i ) {
+				appendChildrenToResponse( response, context );
+			}
+		}
+
+		if( _list != null ) {
+			final List<?> list = (List)_list.valueInComponent( context.component() );
+
+			for( Object object : list ) {
+				_item.setValue( object, context.component() );
+				appendChildrenToResponse( response, context );
+			}
 		}
 	}
 }
