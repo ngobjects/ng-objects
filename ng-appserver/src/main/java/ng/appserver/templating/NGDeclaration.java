29d463b2086facc3ced4a6031e25a7f406335f88
Hugi Thordarson <hugi@karlmenn.is>
2022-04-25 09:45:03 +0000

    Add javadoc for NGDeclaration

--- ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGDeclaration.java
@@ -6,6 +6,10 @@ import ng.appserver.NGAssociation;
 
 /**
  * Represents a declaration of a dynamic tag
+ *
+ * @param name The declaration's name (used to reference the declaration from the HTML template)
+ * @param type The declaration's type (name of dynamic element or component)
+ * @param associations A Map of associations (bindings) on the declaration
  */
 
 public record NGDeclaration( String name, String type, Map<String, NGAssociation> associations ) {}
