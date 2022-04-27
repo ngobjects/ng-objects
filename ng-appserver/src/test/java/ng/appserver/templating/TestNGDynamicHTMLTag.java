a8b808e65343d79e65d54b12d57693ff15202d59
Hugi Thordarson <hugi@karlmenn.is>
2022-04-27 07:42:01 +0000

    Add a rudimentary test for NGDynamicHTMLTag

---
+++ ng-appserver/src/test/java/ng/appserver/templating/TestNGDynamicHTMLTag.java
@@ -0,0 +1,23 @@
+package ng.appserver.templating;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class TestNGDynamicHTMLTag {
+
+	@Test
+	public void testName() {
+		try {
+			NGDynamicHTMLTag tag1 = new NGDynamicHTMLTag( "<wo name = \"_NGString_4\"", null );
+			assertEquals( "_NGString_4", tag1.name() );
+
+			NGDynamicHTMLTag tag2 = new NGDynamicHTMLTag( "<wo name=\"bleble\"", null );
+			assertEquals( "bleble", tag2.name() );
+		}
+		catch( NGHelperFunctionHTMLFormatException e ) {
+			fail();
+		}
+	}
+}
