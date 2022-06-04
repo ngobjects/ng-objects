f1a93cc32fe72c1658187a670f08f494149524f1
Hugi Thordarson <hugi@karlmenn.is>
2022-06-04 13:40:35 +0000

    Experimental wrapper component

---
+++ ng-testapp/src/main/java/ng/testapp/components/WrapperComponent.java
@@ -0,0 +1,11 @@
+package ng.testapp.components;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class WrapperComponent extends NGComponent {
+
+	public WrapperComponent( NGContext context ) {
+		super( context );
+	}
+}
