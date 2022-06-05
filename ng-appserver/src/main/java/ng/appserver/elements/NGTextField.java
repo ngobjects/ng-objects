76d119dd30fc6c2ec67f590121fcaffd83ac8797
Hugi Thordarson <hugi@karlmenn.is>
2022-06-05 12:04:23 +0000

    Add NGTextField's name binding

--- ng-appserver/src/main/java/ng/appserver/elements/NGTextField.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGTextField.java
@@ -10,19 +10,40 @@ import ng.appserver.NGResponse;
 
 public class NGTextField extends NGDynamicElement {
 
+	/**
+	 * 'name' attribute of the text field. If not specified, will be populated using the elementID
+	 */
+	private NGAssociation _nameAssociation;
+
 	public NGTextField( String name, Map<String, NGAssociation> associations, NGElement template ) {
 		super( name, associations, template );
+		_nameAssociation = associations.get( "name" );
 	}
 
 	@Override
-	public void appendToResponse( NGResponse response, NGContext context ) {
+	public void appendToResponse( final NGResponse response, final NGContext context ) {
 		// FIXME: Using String.format for convenience. We probably want to change that later for performance reasons // Hugi 2022-06-05
-		String tagString = String.format( "<input type=\"text\" name=\"%s\" />", name() );
+
+		String name;
+
+		if( _nameAssociation != null ) {
+			name = (String)_nameAssociation.valueInComponent( context.component() );
+		}
+		else {
+			name = nameFromCurrentElementId( context );
+		}
+
+		final String tagString = String.format( "<input type=\"text\" name=\"%s\" />", name );
 		response.appendContentString( tagString );
 
 	}
 
-	private Object name() {
-		return null;
+	/**
+	 * @return A unique name for this text field, based on the WOContext's elementId
+	 *
+	 * FIXME: Implement
+	 */
+	private String nameFromCurrentElementId( final NGContext context ) {
+		return "nameThatShouldHaveBeenMadeFromElementId";
 	}
 }
