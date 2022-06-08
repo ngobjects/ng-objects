a16ea8c36c2b2f3afd8e71ffa866187de2bff6e4
Hugi Thordarson <hugi@karlmenn.is>
2022-06-08 20:45:42 +0000

    Use our NGContext._originalContext for testing, after all, it's there

--- ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
@@ -4,9 +4,7 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 
 	@Override
 	public NGResponse handleRequest( NGRequest request ) {
-		final NGContext originalContext = request.context().session().contexts.get( Integer.parseInt( request.context().requestContextID() ) );
-		System.out.println( originalContext.page().getClass().getName() );
-		originalContext.page().takeValuesFromRequest( request, request.context() );
-		return originalContext.page().invokeAction( request, request.context() ).generateResponse();
+		request.context()._originalContext.page().takeValuesFromRequest( request, request.context() );
+		return request.context()._originalContext.page().invokeAction( request, request.context() ).generateResponse();
 	}
 }
