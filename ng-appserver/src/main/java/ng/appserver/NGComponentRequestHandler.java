00484de99bdb2197237456508fcd3aaf477846fa
Hugi Thordarson <hugi@karlmenn.is>
2022-06-09 10:06:53 +0000

    Return the original context's page if action results are null

--- ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
@@ -6,6 +6,11 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 	public NGResponse handleRequest( NGRequest request ) {
 		request.context()._originalContext.page().takeValuesFromRequest( request, request.context() );
 		final NGActionResults actionResults = request.context()._originalContext.page().invokeAction( request, request.context() );
+
+		if( actionResults == null ) {
+			return request.context()._originalContext.page().generateResponse();
+		}
+
 		return actionResults.generateResponse();
 	}
 }
