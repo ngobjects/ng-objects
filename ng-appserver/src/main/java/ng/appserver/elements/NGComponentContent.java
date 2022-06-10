98cb37eb983672c13e6d87b59ff941a391053236
Hugi Thordarson <hugi@karlmenn.is>
2022-06-10 17:03:22 +0000

    NGComponentContent must push the parent component it's rendering

--- ng-appserver/src/main/java/ng/appserver/elements/NGComponentContent.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGComponentContent.java
@@ -9,6 +9,10 @@ import ng.appserver.NGDynamicElement;
 import ng.appserver.NGElement;
 import ng.appserver.NGResponse;
 
+/**
+ * FIXME: We need to handle the cases of invokeAction and takeValuesFromRequest here as well // Hugi 2022-06-10
+ */
+
 public class NGComponentContent extends NGDynamicElement {
 
 	public NGComponentContent( String name, Map<String, NGAssociation> associations, NGElement template ) {
@@ -23,7 +27,9 @@ public class NGComponentContent extends NGDynamicElement {
 
 		// FIXME: We also need to append the content of the component itself
 		if( component.contentElement() != null ) {
+			context.setCurrentComponent( component.parent() );
 			component.contentElement().appendToResponse( response, context );
+			context.setCurrentComponent( component );
 		}
 	}
 }
