dbbb88c61a0854461dc4d7bc004d2d230f39e1ae
Hugi Thordarson <hugi@karlmenn.is>
2022-06-12 13:41:19 +0000

    Add comment on NGDynamicElementDescription

--- ng-appserver/src/main/java/ng/appserver/NGDynamicElementDescription.java
+++ ng-appserver/src/main/java/ng/appserver/NGDynamicElementDescription.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import ng.appserver.NGDynamicElementDescription.NGBindingDescription;
 
+/**
+ * Provides a description of a dynamic element, particularly what it's bindings are and how they work.
+ */
+
 public record NGDynamicElementDescription( Class<? extends NGDynamicElement> elementClass, List<String> tagNames, List<NGBindingDescription> bindings, String text ) {
 
 	public record NGBindingDescription( String name, String text ) {
