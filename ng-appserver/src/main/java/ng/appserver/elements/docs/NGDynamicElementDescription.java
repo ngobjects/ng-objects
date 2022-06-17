72252f776db2e8226ca6048449924796734a0b33
Hugi Thordarson <hugi@karlmenn.is>
2022-06-17 15:08:31 +0000

    Add one other dynamic element documentation for testing

--- ng-appserver/src/main/java/ng/appserver/elements/docs/NGDynamicElementDescription.java
+++ ng-appserver/src/main/java/ng/appserver/elements/docs/NGDynamicElementDescription.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ng.appserver.NGDynamicElement;
+import ng.appserver.elements.NGImage;
 import ng.appserver.elements.NGString;
 import ng.appserver.elements.docs.NGDynamicElementDescription.NGBindingDescription;
 
@@ -19,14 +20,22 @@ public record NGDynamicElementDescription( Class<? extends NGDynamicElement> ele
 		final ArrayList<NGDynamicElementDescription> list = new ArrayList<>();
 
 		list.add( new NGDynamicElementDescription(
+				NGImage.class,
+				List.of( "img" ),
+				List.of(
+						new NGBindingDescription( "filename", "Path to a webserver resource" ),
+						new NGBindingDescription( "src", "Same as using an src attribute on a regular img tag" ),
+						new NGBindingDescription( "data", "byte array containing image data" ) ),
+				"Displays an image" ) );
+
+		list.add( new NGDynamicElementDescription(
 				NGString.class,
 				List.of( "str" ),
 				List.of(
 						new NGBindingDescription( "value", "The string's value" ),
 						new NGBindingDescription( "valueWhenEmpty", "A string to show if the string is blank or empty" ) ),
-				"For rendering strings " ) );
+				"Renders to a string in a template. If anything other than a string gets passed to [value] it, toString() will be invoked on it to render it" ) );
 
 		return list;
 	}
-
 }
