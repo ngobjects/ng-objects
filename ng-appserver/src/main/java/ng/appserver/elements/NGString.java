1e6231e59af79eb5793af1dc6688ef75b606b943
Hugi Thordarson <hugi@karlmenn.is>
2022-06-17 10:54:11 +0000

    Added the valueWhenEmpty binding

--- ng-appserver/src/main/java/ng/appserver/elements/NGString.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGString.java
@@ -10,20 +10,34 @@ import ng.appserver.NGResponse;
 
 public class NGString extends NGDynamicElement {
 
-	private NGAssociation _value;
+	/**
+	 * The string's value
+	 */
+	private NGAssociation _valueAss;
+
+	/**
+	 * A value to display if the string is null or empty (length==0)
+	 */
+	private NGAssociation _valueWhenEmptyAss;
 
 	public NGString( final String name, final Map<String, NGAssociation> associations, final NGElement template ) {
 		super( name, associations, template );
-		_value = associations.get( "value" );
+		_valueAss = associations.get( "value" );
+		_valueWhenEmptyAss = associations.get( "valueWhenEmpty" );
 	}
 
 	@Override
 	public void appendToResponse( NGResponse response, NGContext context ) {
-		Object value = _value.valueInComponent( context.component() );
+		Object value = _valueAss.valueInComponent( context.component() );
 
-		// FIXME: Missing valueWhenEmpty binding
 		if( value == null ) {
 			value = "";
+		}
+
+		if( _valueWhenEmptyAss != null ) {
+			if( value == null || (value instanceof String s && s.isEmpty()) ) {
+				value = _valueWhenEmptyAss.valueInComponent( context.component() );
+			}
 		}
 
 		response.appendContentString( value.toString() );
