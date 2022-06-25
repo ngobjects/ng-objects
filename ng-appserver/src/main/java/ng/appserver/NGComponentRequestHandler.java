f5f8f7fb4f63e9e77e115396387661dc4bd6c135
Hugi Thordarson <hugi@karlmenn.is>
2022-06-25 14:24:35 +0000

    Added some debug logging

--- ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
@@ -1,13 +1,24 @@
 package ng.appserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NGComponentRequestHandler extends NGRequestHandler {
+
+	private static Logger logger = LoggerFactory.getLogger( NGComponentRequestHandler.class );
 
 	@Override
 	public NGResponse handleRequest( NGRequest request ) {
+		NGContext c = request.context();
+		logger.debug( "uri: " + request.uri() );
+		logger.debug( "context: {} : {}", c.contextID(), c.page() );
+		logger.debug( "originating context: {} : {}", c._originalContext.contextID(), c._originalContext.page() );
+
 		request.context()._originalContext.page().takeValuesFromRequest( request, request.context() );
 		final NGActionResults actionResults = request.context()._originalContext.page().invokeAction( request, request.context() );
 
 		if( actionResults == null ) {
+			logger.debug( "Action method returned null, invoking generateResponse on the original page" );
 			return request.context()._originalContext.page().generateResponse();
 		}
 
