37dfdd82b3d17d1de3dee48a4393ef82f048dab0
Hugi Thordarson <hugi@karlmenn.is>
2022-07-09 11:55:59 +0000

    Added NGBindingConfigurationException

---
+++ ng-appserver/src/main/java/ng/appserver/NGBindingConfigurationException.java
@@ -0,0 +1,12 @@
+package ng.appserver;
+
+/**
+ * Thrown in the case of bad binding configuration (missing required bindings or an unsupported binding combination)
+ */
+
+public class NGBindingConfigurationException extends RuntimeException {
+
+	public NGBindingConfigurationException( String message ) {
+		super( message );
+	}
+}
