a32bddc249a2f272364df422409e9b97717e992d
Hugi Thordarson <hugi@karlmenn.is>
2022-07-10 12:35:52 +0000

    Fix up the mime type tests

--- ng-appserver/src/test/java/ng/appserver/TestNGMimeTypeDetector.java
+++ ng-appserver/src/test/java/ng/appserver/TestNGMimeTypeDetector.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import ng.appserver.NGResourceRequestHandler.NGMimeTypeDetector;
+import ng.appserver.privates.NGMimeTypeDetector;
 
 public class TestNGMimeTypeDetector {
 
