25f2ecc5eb3b1d3e413cc65a281e86cd92e030d8
Hugi Thordarson <hugi@karlmenn.is>
2022-07-10 10:30:35 +0000

    More mimetypes

--- ng-appserver/src/main/java/ng/appserver/privates/NGMimeTypeDetector.java
+++ ng-appserver/src/main/java/ng/appserver/privates/NGMimeTypeDetector.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Mime Type detection.
  *
  * FIXME: This map needs to be extensible // Hugi 2022-04-18
  */
 public class NGMimeTypeDetector {
+
+	private static final Logger logger = LoggerFactory.getLogger( NGMimeTypeDetector.class );
 
 	public static final String mimeTypeForResourceName( final String resourceName ) {
 		Objects.requireNonNull( resourceName );
@@ -23,8 +28,11 @@ public class NGMimeTypeDetector {
 		final String extension = resourceName.substring( lastPeriodIndex + 1 );
 		final String mimeType = mimeTypeForExtension( extension );
 
+		// FIXME:
 		if( mimeType == null ) {
-			throw new IllegalArgumentException( "For some reason, our advanced algorithm has not managed to identify your resource type" );
+			//			throw new IllegalArgumentException( "For some reason, our advanced algorithm has not managed to identify your resource type" );
+			logger.warn( "Unknown file type %s, returning default mimeType".formatted( extension ) );
+			return "application/octet-stream";
 		}
 
 		return mimeType;
@@ -44,6 +52,15 @@ public class NGMimeTypeDetector {
 		map.put( "js", "text/javascript" );
 		map.put( "png", "image/png" );
 		map.put( "css", "text/css" );
+		map.put( "htm", "text/html" );
+		map.put( "html", "text/html" );
+		map.put( "svg", "image/svg+xml" );
+		map.put( "ttf", "application/x-font-ttf" );
+		map.put( "otf", "application/x-font-opentype" );
+		map.put( "woff", "application/font-woff" );
+		map.put( "woff2", "application/font-woff2" );
+		map.put( "eot", "application/vnd.ms-fontobject" );
+		map.put( "sfnt", "application/font-sfnt" );
 		return map;
 	}
 }
