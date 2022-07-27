a017b48c7c6d3b41c0cac8d0723e789070ff4be6
Hugi Thordarson <hugi@karlmenn.is>
2022-07-27 07:40:37 +0000

    Add a comment on NGForwarException

--- ng-appserver/src/main/java/ng/appserver/NGForwardException.java
+++ ng-appserver/src/main/java/ng/appserver/NGForwardException.java
@@ -1,5 +1,9 @@
 package ng.appserver;
 
+/**
+ * FIXME: Would it be a good idea to allow the user to specify a status code along with the exception thrown or even a component name/custom response?
+ */
+
 public class NGForwardException extends RuntimeException {
 
 	public NGForwardException( Exception e ) {
