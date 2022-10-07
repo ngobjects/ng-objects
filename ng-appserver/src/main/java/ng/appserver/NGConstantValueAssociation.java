0afb717145130c5ad2d056143f798630cdab7b76
Hugi Thordarson <hugi@karlmenn.is>
2022-10-07 10:15:01 +0000

    Implement hashCode()/equals() on NGConstantValueAssociation for testing

--- ng-appserver/src/main/java/ng/appserver/NGConstantValueAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/NGConstantValueAssociation.java
@@ -1,5 +1,7 @@
 package ng.appserver;
 
+import java.util.Objects;
+
 public class NGConstantValueAssociation extends NGAssociation {
 
 	private final Object _value;
@@ -19,5 +21,25 @@ public class NGConstantValueAssociation extends NGAssociation {
 	@Override
 	public String toString() {
 		return "[" + getClass().getSimpleName() + ":" + _value + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( _value );
+	}
+
+	@Override
+	public boolean equals( Object obj ) {
+		if( this == obj ) {
+			return true;
+		}
+		if( obj == null ) {
+			return false;
+		}
+		if( getClass() != obj.getClass() ) {
+			return false;
+		}
+		NGConstantValueAssociation other = (NGConstantValueAssociation)obj;
+		return Objects.equals( _value, other._value );
 	}
 }
