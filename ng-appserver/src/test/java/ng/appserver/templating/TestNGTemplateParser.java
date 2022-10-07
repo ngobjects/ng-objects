0afa4ff8d8b65cb42cd6ab63900f508803fc23ae
Hugi Thordarson <hugi@karlmenn.is>
2022-10-07 10:27:20 +0000

    Add skeleton for more dynamic element testing (And some thoughts)

--- ng-appserver/src/test/java/ng/appserver/templating/TestNGTemplateParser.java
+++ ng-appserver/src/test/java/ng/appserver/templating/TestNGTemplateParser.java
@@ -15,7 +15,7 @@ import ng.appserver.elements.NGString;
 public class TestNGTemplateParser {
 
 	@Test
-	public void parse() {
+	public void parseSimpleDynamicElement() {
 		try {
 			Map<String, NGAssociation> associations = Map.of( "value", new NGConstantValueAssociation( "smu" ) );
 			NGElement expected = new NGString( null, associations, null );
@@ -26,4 +26,22 @@ public class TestNGTemplateParser {
 			throw new RuntimeException( e );
 		}
 	}
+
+	/*
+	 * FIXME: To be able to test a container element like NGConditional, equivalence of contained template (which comes from NGDynamicGroup) should be checked as well. Or should it? // Hugi 2022-10-07
+	 *
+		@Test
+		public void parseDynamicContainerElement() {
+			try {
+				NGHTMLBareString containedString = new NGHTMLBareString( "Content textsmu" );
+				Map<String, NGAssociation> associations = Map.of( "condition", NGConstantValueAssociation.TRUE );
+				NGElement expected = new NGConditional( null, associations, containedString );
+				NGElement result = NGTemplateParser.parse( "<wo:if condition=\"$true\">Content text</wo:if>", "", Collections.emptyList() );
+				assertEquals( expected, result );
+			}
+			catch( ClassNotFoundException | NGDeclarationFormatException | NGHTMLFormatException e ) {
+				throw new RuntimeException( e );
+			}
+		}
+		*/
 }
