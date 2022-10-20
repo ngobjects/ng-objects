6df4f18042bfbd2518f6c265e6ef750e05c163ab
Hugi Thordarson <hugi@karlmenn.is>
2022-10-20 06:31:02 +0000

    Delete unused NGAdaptor.dispatchRequest method

--- ng-appserver/src/main/java/ng/appserver/NGAdaptor.java
+++ ng-appserver/src/main/java/ng/appserver/NGAdaptor.java
@@ -6,11 +6,4 @@ public abstract class NGAdaptor {
 	 * Start an instance of the adaptor in the given application
 	 */
 	public abstract void start( NGApplication application );
-
-	/**
-	 * FIXME: Added as a placeholder until I decide the best way to pass the request on to the actual request handling mechanism
-	 */
-	public NGResponse dispatchRequest( final NGRequest request ) {
-		return NGApplication.application().dispatchRequest( request );
-	}
 }
