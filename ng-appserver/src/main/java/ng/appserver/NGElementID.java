49a3a5a7632326ea61e99173b512d5b202444349
Hugi Thordarson <hugi@karlmenn.is>
2022-12-30 21:29:00 +0000

    Implement equals() and hashCode() on NGElementID

--- ng-appserver/src/main/java/ng/appserver/NGElementID.java
+++ ng-appserver/src/main/java/ng/appserver/NGElementID.java
@@ -2,6 +2,7 @@ package ng.appserver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents an elementID.
@@ -10,11 +11,6 @@ import java.util.List;
 public class NGElementID {
 
 	private final List<Integer> elements = new ArrayList<>();
-
-	@Override
-	public String toString() {
-		return String.join( ".", elements.stream().map( String::valueOf ).toList() );
-	}
 
 	public void addBranch() {
 		elements.add( 0 );
@@ -27,5 +23,20 @@ public class NGElementID {
 	public void increment() {
 		int lastValue = elements.remove( elements.size() - 1 );
 		elements.add( lastValue + 1 );
+	}
+
+	@Override
+	public String toString() {
+		return String.join( ".", elements.stream().map( String::valueOf ).toList() );
+	}
+
+	@Override
+	public boolean equals( Object obj ) {
+		return Objects.equals( this, obj );
+	}
+
+	@Override
+	public int hashCode() {
+		return elements.hashCode();
 	}
 }
