056d9159c3838bbe071d10fd32d53ad0f4ec4ffa
Hugi Thordarson <hugi@karlmenn.is>
2023-01-07 08:41:42 +0000

    Removed debugging code from NGComponentRequestHandler

--- ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponentRequestHandler.java
@@ -12,19 +12,11 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 
 	@Override
 	public NGResponse handleRequest( NGRequest request ) {
-		// at this point, this point, the request's context is a freshly created one
+		// At this point, this point, the request's context is a freshly created one
 		final NGContext context = request.context();
 
-		//		logger.debug( "request.context: " + context );
-		//		logger.debug( "request.originatingContext: " + context.originatingContext() );
-		logger.debug( "pageCache: " + _pageCache );
-
-		final String pageKey = pageCacheKey( context.originatingContext().contextID(), context.senderID().toString() );
-
-		logger.debug( "Our pageKey is: " + pageKey );
-
 		// Now let's try to restore the page from the cache
-		final NGComponent originalPage = restorePageFromCache( pageKey );
+		final NGComponent originalPage = restorePageFromCache( context.originatingContext().contextID() );
 
 		if( originalPage == null ) {
 			throw new IllegalStateException( "No page found in cache" );
@@ -42,28 +34,23 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 
 		logger.debug( "About to perform invokeAction on element {} in context {} on page {} ", context.senderID(), context.originatingContext().contextID(), originalPage );
 
-		// This is what we got from invoking the action on the original page.
+		// We now invoke the action on the original page instance
 		final NGActionResults actionInvocationResults = originalPage.invokeAction( request, context );
 
 		logger.debug( "Action invocation returned {}", actionInvocationResults );
 
-		// And this is what we're going to eventually return.
-		NGResponse response; // FIXME: Don't initialize to null.
+		// The response we will eventually return
+		NGResponse response;
 
 		// If action results are null, we're returning the same page
 		if( actionInvocationResults == null ) {
 			logger.debug( "Action method returned null, invoking generateResponse on the original page" );
-			//			savePage( context.contextID(), originalPage );
 			response = originalPage.generateResponse();
 		}
 		else if( actionInvocationResults instanceof NGComponent newPage ) {
 			context.setPage( newPage );
 			context.setCurrentComponent( newPage );
 			newPage.awakeInContext( context );
-
-			// Since we've switched pages, we need to save this page in the current context
-			//			savePage( context.contextID(), ngc );
-
 			response = newPage.generateResponse();
 		}
 		else {
@@ -78,16 +65,6 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 		return response;
 	}
 
-	public static String pageCacheKey( String contextID, String senderID ) {
-		String key = contextID;
-
-		//		if( senderID != null ) {
-		//			key = key + "." + senderID;
-		//		}
-
-		return key;
-	}
-
 	/**
 	 * FIXME: OK, this is horrible, but we're going to start out with out pageCache here. This belongs in the session, really.
 	 *
@@ -100,13 +77,13 @@ public class NGComponentRequestHandler extends NGRequestHandler {
 	 */
 	public static Map<String, NGComponent> _pageCache = new HashMap<>();
 
-	public static void savePage( String key, NGComponent component ) {
-		logger.debug( "Saving page {} in cache with key {} ", component.getClass(), key );
-		_pageCache.put( key, component );
+	public static void savePage( String contextID, NGComponent component ) {
+		logger.debug( "Saving page {} in cache with contextID {} ", component.getClass(), contextID );
+		_pageCache.put( contextID, component );
 	}
 
-	public static NGComponent restorePageFromCache( String key ) {
-		logger.debug( "Restoring page from cache with key: " + key );
-		return _pageCache.get( key );
+	public static NGComponent restorePageFromCache( String contextID ) {
+		logger.debug( "Restoring page from cache with contextID: " + contextID );
+		return _pageCache.get( contextID );
 	}
 }
