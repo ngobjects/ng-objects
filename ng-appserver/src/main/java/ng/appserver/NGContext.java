bdf36ee167425e59b4056737d465c38de0156c43
Hugi Thordarson <hugi@karlmenn.is>
2023-01-21 11:24:18 +0000

    Don't store contexts with the session and keep a counter for contextID

--- ng-appserver/src/main/java/ng/appserver/NGContext.java
+++ ng-appserver/src/main/java/ng/appserver/NGContext.java
@@ -69,10 +69,10 @@ public class NGContext {
 		// FIXME: We're currently creating a session alongside every context. This is horrid // Hugi 2023-01-07
 
 		// Our contextID is just the next free slot in the session's context array
-		_contextID = String.valueOf( session().contexts.size() );
+		//		_contextID = String.valueOf( session().contexts.size() );
 
 		// Store our context with the session
-		session().contexts.add( this );
+		//		session().contexts.add( this );
 
 		if( request.uri().contains( "/wo/" ) ) {
 			final String componentPart = request.parsedURI().getString( 1 );
@@ -156,8 +156,15 @@ public class NGContext {
 
 	/**
 	 * @return ID of the element currently being rendered by the context.
+	 *
+	 * FIXME: Take note of concurrency issues for lazy initialization // Hugi 2023-01-21
+	 * FIXME: Why isn't the contextID an integer? Keeping it a string for now for WO code compaitibility // Hugi 2023-01-21
 	 */
 	public String contextID() {
+		if( _contextID == null ) {
+			_contextID = String.valueOf( session().getContextIDAndIncrement() );
+		}
+
 		return _contextID;
 	}
 
--- ng-appserver/src/main/java/ng/appserver/NGSession.java
+++ ng-appserver/src/main/java/ng/appserver/NGSession.java
@@ -2,9 +2,7 @@ package ng.appserver;
 
 import java.lang.reflect.InvocationTargetException;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -49,8 +47,17 @@ public class NGSession {
 
 	/**
 	 * FIXME: This is not the way we're going to store/cache contexts. Just for testing
+	 * FIXME: I don't think we actually need this? Isn't the page cache really the only thing we need for context storage?
 	 */
-	public final List<NGContext> contexts = new ArrayList<>();
+	//	public final List<NGContext> contexts = new ArrayList<>();
+
+	/**
+	 * Holds our context ID
+	 *
+	 * FIXME: Only public while we're testing this implementation // Hugi 2023-01-21
+	 * FIXME: At what point do we reset the counter? // Hugi 2023-01-21
+	 */
+	public int currentContextID = 0;
 
 	/**
 	 * The page cache is going to have to keep track of
@@ -77,6 +84,10 @@ public class NGSession {
 		_birthDate = birthDate;
 		_lastTouchedDate = birthDate;
 		_timeOutInMilliseconds = 60 * 60 * 1000; // FIXME: Default timeout of one hour. Should be set, probably somewhere in NGApplication (and/or using a property)
+	}
+
+	public int getContextIDAndIncrement() {
+		return ++currentContextID;
 	}
 
 	public String sessionID() {
