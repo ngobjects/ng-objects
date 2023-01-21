8a3004238dcf18a3ec02829afac8d1e4b14b684c
Hugi Thordarson <hugi@karlmenn.is>
2023-01-21 09:14:28 +0000

    Added the overview component

---
+++ ng-control/src/main/java/ng/control/NGOverview.java
@@ -0,0 +1,11 @@
+package ng.control;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class NGOverview extends NGComponent {
+
+	public NGOverview( NGContext context ) {
+		super( context );
+	}
+}
