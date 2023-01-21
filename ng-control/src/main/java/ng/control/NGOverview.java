0f038b35ef02b447989452eb94e3512c645173a4
Hugi Thordarson <hugi@karlmenn.is>
2023-01-21 09:31:33 +0000

    Added an actual session list

--- ng-control/src/main/java/ng/control/NGOverview.java
+++ ng-control/src/main/java/ng/control/NGOverview.java
@@ -1,11 +1,21 @@
 package ng.control;
 
+import java.util.List;
+
+import ng.appserver.NGApplication;
 import ng.appserver.NGComponent;
 import ng.appserver.NGContext;
+import ng.appserver.NGSession;
 
 public class NGOverview extends NGComponent {
 
+	public NGSession currentSession;
+
 	public NGOverview( NGContext context ) {
 		super( context );
+	}
+
+	public List<NGSession> sessions() {
+		return NGApplication.application().sessionStore().sessions();
 	}
 }
