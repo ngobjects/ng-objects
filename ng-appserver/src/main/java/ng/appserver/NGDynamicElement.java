a51d3f258bdb45a67e36efdc8a51e1ae8551eda3
Hugi Thordarson <hugi@karlmenn.is>
2023-02-06 10:03:10 +0000

    Rename template -> contentTemplate for some symmetry in var naming

--- ng-appserver/src/main/java/ng/appserver/NGDynamicElement.java
+++ ng-appserver/src/main/java/ng/appserver/NGDynamicElement.java
@@ -20,9 +20,9 @@ public abstract class NGDynamicElement implements NGElement {
 	/**
 	 * The constructor all dynamic elements must use. In the case of this class, it's a no-op so for sanity, we ensure we're always passing in null for all arguments
 	 */
-	public NGDynamicElement( final String name, final Map<String, NGAssociation> associations, final NGElement template ) {
+	public NGDynamicElement( final String name, final Map<String, NGAssociation> associations, final NGElement contentTemplate ) {
 
-		if( name != null || associations != null || template != null ) {
+		if( name != null || associations != null || contentTemplate != null ) {
 			logger.warn( getClass().getSimpleName() + ": [name], [associations] or [template] was not null. This constructor should only be invoked with null parameters" );
 			// FIXME: Since this is a weird state, we should probably be throwing here instead of logging. Babysteps. // Hugi 2022-07-09
 			// throw new IllegalArgumentException( "name, associations or template was not null. This constructor should only be invoked with null parameters" );
