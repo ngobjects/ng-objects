e2dc83cfed7c409e903bb9bf23e73817b07c3eb1
Hugi Thordarson <hugi@karlmenn.is>
2023-02-07 20:22:44 +0000

    More sensible structure in NGDirectAction initialization

--- ng-appserver/src/main/java/ng/appserver/directactions/NGDirectAction.java
+++ ng-appserver/src/main/java/ng/appserver/directactions/NGDirectAction.java
@@ -13,19 +13,19 @@ import ng.appserver.NGResponse;
 
 public class NGDirectAction {
 
-	private final NGContext _context;
+	private final NGRequest _request;
 
 	public NGDirectAction( final NGRequest request ) {
 		Objects.requireNonNull( request );
-		_context = request.context();
+		_request = request;
 	}
 
 	public NGContext context() {
-		return _context;
+		return _request.context();
 	}
 
 	public NGRequest request() {
-		return context().request();
+		return _request;
 	}
 
 	public <E extends NGComponent> E pageWithName( final Class<E> componentClass ) {
