1b6c18649813c4cda2ebc7cc68a95ab95f3e0c2c
Hugi Thordarson <hugi@karlmenn.is>
2023-03-10 10:19:31 +0000

    Show a list of all property keys

--- ng-control/src/main/java/ng/control/NGOverview.java
+++ ng-control/src/main/java/ng/control/NGOverview.java
@@ -1,6 +1,9 @@
 package ng.control;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import ng.appserver.NGActionResults;
 import ng.appserver.NGComponent;
@@ -10,6 +13,7 @@ import ng.appserver.NGSession;
 public class NGOverview extends NGComponent {
 
 	public NGSession currentSession;
+	public Object currentPropertyKey;
 
 	public NGOverview( NGContext context ) {
 		super( context );
@@ -26,5 +30,19 @@ public class NGOverview extends NGComponent {
 	public NGActionResults terminate() {
 		currentSession.terminate();
 		return null;
+	}
+
+	public Map<Object, Object> properties() {
+		return System.getProperties();
+	}
+
+	public List<String> propertyKeys() {
+		final List<String> list = new ArrayList( System.getProperties().keySet() );
+		list.sort( Comparator.naturalOrder() );
+		return list;
+	}
+
+	public Object currentPropertyValue() {
+		return properties().get( currentPropertyKey );
 	}
 }
