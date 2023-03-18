e2bd696766cea5c620efa44f8546a7b7af864a89
Hugi Thordarson <hugi@karlmenn.is>
2023-03-18 10:48:40 +0000

    Add a little more testing for NGKeyValueCodingAdditions throwing

--- ng-core/src/test/java/ng/kvc/TestNGKeyValueCodingAdditions.java
+++ ng-core/src/test/java/ng/kvc/TestNGKeyValueCodingAdditions.java
@@ -2,8 +2,11 @@ package ng.kvc;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import ng.kvc.NGKeyValueCoding.UnknownKeyException;
 
 public class TestNGKeyValueCodingAdditions {
 
@@ -12,6 +15,23 @@ public class TestNGKeyValueCodingAdditions {
 		var p = new Person( "Hugi", new Address( "Reykjavík" ) );
 		assertEquals( 4, NGKeyValueCodingAdditions.DefaultImplementation.valueForKeyPath( p, "name.toLowerCase.toUpperCase.toLowerCase.length" ) );
 		assertEquals( "Reykjavík", NGKeyValueCodingAdditions.DefaultImplementation.valueForKeyPath( p, "address.city" ) );
+	}
+
+	@Test
+	public void valueForKeyPathThrowsUnknownKeyException() {
+		var p = new Person( "Hugi", new Address( "Reykjavík" ) );
+
+		assertThrows( UnknownKeyException.class, () -> {
+			NGKeyValueCodingAdditions.DefaultImplementation.valueForKeyPath( p, "bla.smu.fne" );
+		} );
+
+		assertThrows( UnknownKeyException.class, () -> {
+			NGKeyValueCodingAdditions.DefaultImplementation.valueForKeyPath( p, "address.citi" );
+		} );
+
+		assertThrows( UnknownKeyException.class, () -> {
+			NGKeyValueCodingAdditions.DefaultImplementation.valueForKeyPath( p, "adres.city" );
+		} );
 	}
 
 	@Test
