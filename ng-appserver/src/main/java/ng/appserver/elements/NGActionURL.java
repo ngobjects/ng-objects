379997f5eca08aa2a7d1b2bbc12521a06aa1dde2
Hugi Thordarson <hugi@karlmenn.is>
2023-06-20 19:19:03 +0000

    Initial implementation of NGActionURL

---
+++ ng-appserver/src/main/java/ng/appserver/elements/NGActionURL.java
@@ -0,0 +1,40 @@
+package ng.appserver.elements;
+
+import java.util.Map;
+
+import ng.appserver.NGActionResults;
+import ng.appserver.NGAssociation;
+import ng.appserver.NGBindingConfigurationException;
+import ng.appserver.NGContext;
+import ng.appserver.NGDynamicElement;
+import ng.appserver.NGElement;
+import ng.appserver.NGRequest;
+import ng.appserver.NGResponse;
+
+public class NGActionURL extends NGDynamicElement {
+
+	private final NGAssociation _actionAssociation;
+
+	public NGActionURL( String name, Map<String, NGAssociation> associations, NGElement contentTemplate ) {
+		super( null, null, null );
+		_actionAssociation = associations.get( "action" );
+
+		if( _actionAssociation == null ) {
+			throw new NGBindingConfigurationException( "'action' is a required binding" );
+		}
+	}
+
+	@Override
+	public void appendToResponse( NGResponse response, NGContext context ) {
+		response.appendContentString( context.componentActionURL() );
+	}
+
+	@Override
+	public NGActionResults invokeAction( NGRequest request, NGContext context ) {
+		if( context.currentElementIsSender() ) {
+			return (NGActionResults)_actionAssociation.valueInComponent( context.component() );
+		}
+
+		return null;
+	}
+}
