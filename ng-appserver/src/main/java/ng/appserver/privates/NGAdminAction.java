ddcca0ee41f480622f49c275be877df2e73517cd
Hugi Thordarson <hugi@karlmenn.is>
2023-07-01 09:40:49 +0000

    Add access control and docs on NGAdminAction

--- ng-appserver/src/main/java/ng/appserver/privates/NGAdminAction.java
+++ ng-appserver/src/main/java/ng/appserver/privates/NGAdminAction.java
@@ -6,6 +6,10 @@ import ng.appserver.NGRequest;
 import ng.appserver.NGResponse;
 import ng.appserver.directactions.NGDirectAction;
 
+/**
+ * Some administrative actions for use in development
+ */
+
 public class NGAdminAction extends NGDirectAction {
 
 	public NGAdminAction( NGRequest request ) {
@@ -13,11 +17,29 @@ public class NGAdminAction extends NGDirectAction {
 	}
 
 	/**
-	 * FIXME: We probably want to return a structured response here, a la WOMPRequestHandler
-	 * FIXME: We probably want to control access to this. Having a public kill switch isn't perhaps… bright.
+	 * Overridden to control access
+	 */
+	@Override
+	public NGActionResults performActionNamed( String directActionName ) {
+		if( NGApplication.application().isDevelopmentMode() ) {
+			return super.performActionNamed( directActionName );
+		}
+
+		return new NGResponse( "Admin actions can only be used in development mode", 403 );
+	}
+
+	/**
+	 * Terminates this application instance and returns a 200 response
 	 */
 	public NGActionResults terminateAction() {
 		NGApplication.application().terminate();
 		return new NGResponse( "Application successfully terminated", 200 );
+	}
+
+	/**
+	 * @return Just a simple string to indicate that this is an NGObjects application
+	 */
+	public NGActionResults typeAction() {
+		return new NGResponse( "ng", 200 );
 	}
 }
