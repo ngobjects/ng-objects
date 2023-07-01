7890d7c185b7125b083e10768d836e6465a28714
Hugi Thordarson <hugi@karlmenn.is>
2023-07-01 16:48:22 +0000

    Change WOElement's FIXME to CHECKME

--- ng-appserver/src/main/java/com/webobjects/appserver/WOElement.java
+++ ng-appserver/src/main/java/com/webobjects/appserver/WOElement.java
@@ -1,7 +1,7 @@
 package com.webobjects.appserver;
 
 /**
- * FIXME:
+ * CHECKME:
  * This interface exists solely to fool WOLips into thinking it's working with WO, thus
  * allowing it to locate component class files and perform binding autocompletion in the component editor
  * Delete once we've done some work on the template editor side to support NGElement.
