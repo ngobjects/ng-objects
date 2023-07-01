3011a4886c8a7d84d71184095be2c30ef2b80efc
Hugi Thordarson <hugi@karlmenn.is>
2023-07-01 10:27:41 +0000

    Ad documentation for NGForm

--- ng-appserver/src/main/java/ng/appserver/elements/docs/NGDynamicElementDescription.java
+++ ng-appserver/src/main/java/ng/appserver/elements/docs/NGDynamicElementDescription.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
 
+import ng.appserver.NGActionResults;
 import ng.appserver.NGDynamicElement;
 import ng.appserver.elements.NGComponentContent;
 import ng.appserver.elements.NGConditional;
@@ -90,7 +91,13 @@ public record NGDynamicElementDescription( Class<? extends NGDynamicElement> ele
 						new NGBindingDescription( "negate", Boolean.class, "Can be set to $true to 'flip' the condition" ) ),
 				"Wraps content in a template and decides to render it based on a condition. If the binding [condition] evaluates to $false, the contained content will not be rendered (and vice versa). If the 'negate' binding is set to $true, the condition will be flipped." ) );
 
-		list.add( createEmptyDescription( NGForm.class ) );
+		list.add( new NGDynamicElementDescription(
+				NGForm.class,
+				List.of( "form" ),
+				List.of(
+						new NGBindingDescription( "action", NGActionResults.class, "The action to invoke when the form is submitted" ),
+						new NGBindingDescription( "method", String.class, "The form's method (POST or GET). Defaults to 'POST'" ) ),
+				"An HTML form, specifically for use with component actions" ) );
 		list.add( createEmptyDescription( NGGenericContainer.class ) );
 		list.add( createEmptyDescription( NGGenericElement.class ) );
 		list.add( createEmptyDescription( NGHyperlink.class ) );
