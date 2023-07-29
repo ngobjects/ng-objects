a0780938824dd6dab248e1c3c92271ddf1ef1549
Hugi Thordarson <hugi@karlmenn.is>
2023-07-29 10:15:51 +0000

    We don't have a concept of read-only keyPaths no need for error handling

--- ng-appserver/src/main/java/ng/appserver/NGAssociationFactory.java
+++ ng-appserver/src/main/java/ng/appserver/NGAssociationFactory.java
@@ -12,18 +12,6 @@ public class NGAssociationFactory {
 			return new NGBindingAssociation( keyPath.substring( 1 ) );
 		}
 
-		if( keyPathIsReadOnly( keyPath ) ) {
-			throw new RuntimeException( "Read only keypath associations are not supported" );
-		}
-
 		return new NGKeyValueAssociation( keyPath );
-	}
-
-	/**
-	 * This represents a keypath that contains an operator (@).
-	 * Our KVC implementation currently does not support operators but I'm keeping the functionality around for a bit while I mull this over
-	 */
-	private static boolean keyPathIsReadOnly( String keyPath ) {
-		return keyPath.startsWith( "@" ) || keyPath.indexOf( ".@" ) > 0;
 	}
 }
