20b9e8bc4ae4a6933e6afeb93fb401c71f2ce200
Hugi Thordarson <hugi@karlmenn.is>
2023-09-25 07:37:32 +0000

    Add the initial skeleton for a "browser" element

--- ng-appserver/src/main/java/ng/appserver/templating/NGElementUtils.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGElementUtils.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import ng.appserver.NGAssociation;
 import ng.appserver.NGElement;
 import ng.appserver.elements.NGActionURL;
+import ng.appserver.elements.NGBrowser;
 import ng.appserver.elements.NGComponentContent;
 import ng.appserver.elements.NGConditional;
 import ng.appserver.elements.NGForm;
@@ -57,6 +58,7 @@ public class NGElementUtils {
 
 	static {
 		addClass( NGActionURL.class, "actionURL" );
+		addClass( NGBrowser.class, "browser" );
 		addClass( NGComponentContent.class, "content" );
 		addClass( NGConditional.class, "if" );
 		addClass( NGForm.class, "form" );
