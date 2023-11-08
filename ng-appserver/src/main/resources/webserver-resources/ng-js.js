9ed0d61a6f972e4ee0fc7b5dec057bf52b048320
Hugi Thordarson <hugi@karlmenn.is>
2023-11-08 15:16:45 +0000

    Add the JS stuff for Ajax

---
+++ ng-appserver/src/main/resources/webserver-resources/ng-js.js
@@ -0,0 +1,52 @@
+var AUC = {
+	register: function(id, options) {
+		if (!options) {
+			options = {};
+		}
+		eval(id + "Update = function() {AjaxUpdateContainer.update(id, options) }");
+	}
+}
+
+var AUL = {
+	update: function(id, options, elementID, queryParams) {
+		
+		// Just some logging. For fun.
+		console.log( "===== Clicked AjaxUpdateLink =====")
+		console.log( "id: " + id );
+		console.log( "options: " + options );
+		console.log( "elementID: " + elementID );
+		console.log( "queryParams: " + queryParams );
+		
+		// This is the updateContainer we're going to target
+		var updateContainer = document.getElementById(id);
+		
+		
+//		if( updateContainer ) {
+//			alert('No AjaxUpdateContainer on the page with id ' + id);
+//		}
+
+		var actionUrl = updateContainer.getAttribute('data-updateUrl');
+
+		// We cleverly replace the elementID on the UC to the clicked link's element ID
+		actionUrl = actionUrl.replace(/[^\/]+$/, elementID);
+		actionUrl = actionUrl + '?_u=' + id;
+		invokeUpdate( id, actionUrl );
+	}
+}
+
+function invokeUpdate( id, url ) {
+	const xhttp = new XMLHttpRequest();
+	xhttp.open("GET", url, true);
+	xhttp.setRequestHeader( 'x-updatecontainerid', 'smu')
+	
+	xhttp.onload = () => {
+		var updateContainer = document.getElementById(id);
+		updateContainer.innerHTML = xhttp.responseText;
+    };
+	
+	xhttp.send();
+}
+
+function updateSmu( url ) {
+	invokeUpdate( 'smu', url );
+}
