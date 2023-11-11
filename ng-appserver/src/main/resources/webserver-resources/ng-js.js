ca8b199052195bdadf3f3a6fd5ec538f3e026827
Hugi Thordarson <hugi@karlmenn.is>
2023-11-11 14:08:34 +0000

    Some additions to ng-js

--- ng-appserver/src/main/resources/webserver-resources/ng-js.js
+++ ng-appserver/src/main/resources/webserver-resources/ng-js.js
@@ -2,21 +2,56 @@ function invokeUpdate( id, url ) {
 	const xhttp = new XMLHttpRequest();
 	xhttp.open("GET", url, true);
 	xhttp.setRequestHeader( 'x-updatecontainerid', id )
-	
-	xhttp.onload = () => {
-		var updateContainer = document.getElementById(id);
-		
-		if( !updateContainer ) {
-			// CHECKME: WE should probably abandon the whole operation if there UC is missing
-			alert( 'No AjaxUpdateContainer on the page with id ' + id );
-		}
 
-		updateContainer.innerHTML = xhttp.responseText;
-    };
+	// FIXME: ugly as all hell, just here for some testing
+	if( id != 'null') {
+		xhttp.onload = () => {
+			var updateContainer = document.getElementById(id);
+			
+			if( !updateContainer ) {
+				// CHECKME: WE should probably abandon the whole operation if there UC is missing
+				alert( 'No AjaxUpdateContainer on the page with id ' + id );
+			}
+	
+			updateContainer.innerHTML = xhttp.responseText;
+	    };
+	}
 	
 	xhttp.send();
 }
 
+function performSubmit( form ) {
+    var data = new FormData(form);
+    
+    // Add extra data to form if required submission.
+    // data.append("referer","https://example.com");
+
+	// Obtain the form's action url for use when submitting    
+	const uri = form.getAttribute('action');
+
+    var xhr = new XMLHttpRequest();
+	xhr.open('POST',uri);
+    xhr.send(data);
+
+	console.log( data );
+
+	/*  
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            form.reset(); //reset form after AJAX success.
+        }
+    }
+    */
+}
+
+/* 
+function registerForm( form ) {
+    form.onsubmit = function(event){
+		 
+    }
+}
+ */
+ 
 /*  
 var AUC = {
 	register: function(id, options) {
