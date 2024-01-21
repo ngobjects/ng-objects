fcd43f1abfb923fdc2094af4d709ec6b72a386bb
Hugi Thordarson <hugi@karlmenn.is>
2024-01-21 11:20:08 +0000

    Added NGWelcomePage (a nicer initial response for a new application)

---
+++ ng-appserver/src/main/java/x/junk/NGWelcomePage.java
@@ -0,0 +1,11 @@
+package x.junk;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class NGWelcomePage extends NGComponent {
+
+	public NGWelcomePage( NGContext context ) {
+		super( context );
+	}
+}
