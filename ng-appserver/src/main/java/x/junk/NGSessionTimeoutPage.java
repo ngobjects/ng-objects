b6503218000543a144070e9227c7e06389248451
Hugi Thordarson <hugi@karlmenn.is>
2024-03-28 18:10:38 +0000

    Add note on NGSessionTimeoutPage

--- ng-appserver/src/main/java/x/junk/NGSessionTimeoutPage.java
+++ ng-appserver/src/main/java/x/junk/NGSessionTimeoutPage.java
@@ -6,6 +6,7 @@ import ng.appserver.NGSessionRestorationException;
 
 public class NGSessionTimeoutPage extends NGComponent {
 
+	// FIXME: This variable, while set, is never actually used. Not sure we should keep it around // Hugi 2024-03-28
 	private NGSessionRestorationException _exception;
 
 	public NGSessionTimeoutPage( NGContext context ) {
