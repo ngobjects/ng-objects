1a6b602ac15541180711667c663d90f3665a5a31
Hugi Thordarson <hugi@karlmenn.is>
2024-08-05 09:23:41 +0000

    We've already eliminated the AjaxResponse class, FIXME removed

--- ng-appserver/src/main/java/ng/appserver/NGComponent.java
+++ ng-appserver/src/main/java/ng/appserver/NGComponent.java
@@ -226,7 +226,6 @@ public class NGComponent implements NGElement, NGActionResults {
 		context().setPage( this );
 		context().setComponent( this );
 
-		// FIXME: We should only be constructing an AjaxResponse if the request is an AjaxRequest
 		final NGResponse response = new NGResponse();
 		response.setHeader( "content-type", "text/html;charset=utf-8" ); // FIXME: This is most definitely not the place to set the encoding // Hugi 2023-03-12
 
