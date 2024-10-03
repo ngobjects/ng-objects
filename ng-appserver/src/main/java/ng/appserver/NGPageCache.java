db841eb74a5b83cfa8dca969ba819c868b301a9b
Hugi Thordarson <hugi@karlmenn.is>
2024-10-03 08:14:33 +0000

    Rewrote page cache to store partial page update with the containing page

--- ng-appserver/src/main/java/ng/appserver/NGPageCache.java
+++ ng-appserver/src/main/java/ng/appserver/NGPageCache.java
@@ -1,8 +1,7 @@
 package ng.appserver;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -10,10 +9,6 @@ import org.slf4j.LoggerFactory;
 
 /**
  * The page cache is used by stateful actions to store instances of previously rendered components
- *
- * FIXME: Evacuate all children from page cache when parent is evacuated // Hugi 2024-09-30
- * FIXME: Retain parent entry when a child entry is retained // Hugi 2024-09-30
- * FIXME: The stored map should probably only contain root entries, using a different map to map to "all entries" // Hugi 2024-09-30
  */
 
 public class NGPageCache {
@@ -24,17 +19,32 @@ public class NGPageCache {
 	 * Represents a single entry in the page cache, along with it's "child entries"
 	 * "Child entries" currently means entries generated for update containers within the same page, meaning they can be thrown out with their parent.
 	 */
-	public record NGPageCacheEntry( String contextID, NGComponent page, String originatingContextID, String updateContainerID, List<NGPageCacheEntry> children ) {
+	public record NGPageCacheEntry( String contextID, NGComponent page, String originatingContextID, String updateContainerID, Map<String, NGPageCacheEntry> children ) {
 
 		public NGPageCacheEntry( String contextID, NGComponent page, String originatingContextID, String updateContainerID ) {
-			this( contextID, page, originatingContextID, updateContainerID, new ArrayList<>() );
+			this( contextID, page, originatingContextID, updateContainerID, new LinkedHashMap<>() );
+		}
+
+		/**
+		 * @return true if this was a partial page update
+		 */
+		public boolean isPartial() {
+			// FIXME: nulls are not nice. A full page update should probably be represented by explicit values for both the originating context and the updateContainer // Hugi 2024-09-30
+			return originatingContextID != null && updateContainerID != null;
 		}
 	}
 
 	/**
-	 * In the case of component actions, stores the currently active page instance by contextID.
+	 * Root map, stores only full page updates
 	 */
 	private Map<String, NGPageCacheEntry> _cacheMap = new LinkedHashMap<>();
+
+	/**
+	 * Maps all contextIDs, regardless of whether they're full page updates or partial updates
+	 *
+	 * FIXME: I don't like having this around, we're going to have to sit down and make a better design of this // Hugi 2024-10-03
+	 */
+	private Map<String, NGPageCacheEntry> _allEntries = new HashMap<>();
 
 	/**
 	 * @return Size of the page cache
@@ -54,7 +64,7 @@ public class NGPageCache {
 	 * @param updateContainerID The ID of the update container targeted by the given request. We only need one cached copy for each update container (since an area's content will never get used once it's been replaced)
 	 */
 	public void savePage( final String contextID, final NGComponent page, final String originatingContextID, final String updateContainerID ) {
-		logger.debug( "Saving page '{}' in cache with contextID '{}' originating from context '{}', updateContainerID '{}'", page.getClass(), contextID, originatingContextID, updateContainerID );
+		logger.debug( "Saving contextID '{}' with page '{}' originating from context '{}', updateContainerID '{}'", contextID, page.getClass(), originatingContextID, updateContainerID );
 
 		// A little sanity check since if we're storing the same contextID twice, we're probably on our way to do something horrible
 		if( _cacheMap.containsKey( contextID ) ) {
@@ -62,24 +72,41 @@ public class NGPageCache {
 		}
 
 		final NGPageCacheEntry cacheEntry = new NGPageCacheEntry( contextID, page, originatingContextID, updateContainerID );
-		_cacheMap.put( contextID, cacheEntry );
 
-		// If the updateContainerID is not null, we're going to associate this entry with it's parent.
-		// FIXME: nulls are not nice. A full page update should probably be represented by an explicit values for both the originating context and the updateContainer // Hugi 2024-09-30
-		if( originatingContextID != null && updateContainerID != null ) {
+		// In case of partial updates, the cache entry will get stored with it's parent entry, keyed by the ID of the updateContainer
+		if( cacheEntry.isPartial() ) {
 			final NGPageCacheEntry parentEntry = _cacheMap.get( originatingContextID );
-			parentEntry.children().add( parentEntry );
+			parentEntry.children().put( cacheEntry.updateContainerID(), cacheEntry );
+		}
+		else {
+			_cacheMap.put( contextID, cacheEntry );
+
+			// If the page cache size has been reached, remove the oldest entry
+			if( _cacheMap.size() > pageCacheSize() ) {
+				// Since the page cache is a LinkedHashMap (which maintains insertion order), the first entry should be the oldest one
+				final NGPageCacheEntry oldestEntry = _cacheMap.values().iterator().next();
+				removeEntry( oldestEntry );
+			}
 		}
 
-		// If the page cache size has been reached, remove the oldest entry
-		if( _cacheMap.size() > pageCacheSize() ) {
-			// Since the page cache is a LinkedHashMap (which maintains insertion order), the first entry should be the oldest one
-			final String oldestEntryKey = _cacheMap.keySet().iterator().next();
+		_allEntries.put( contextID, cacheEntry );
+	}
 
-			// Bye bye
-			_cacheMap.remove( oldestEntryKey );
-			logger.debug( "Popped contextID {} from page cache", page.getClass(), oldestEntryKey );
-		}
+	/**
+	 * Removes the given entry from the page cache, along with all it's child entries
+	 */
+	private void removeEntry( NGPageCacheEntry entry ) {
+		logger.debug( "Removing contextID '{}' with page '{}' originating from context '{}', updateContainerID '{}'", entry.contextID(), entry.page().getClass(), entry.originatingContextID(), entry.updateContainerID() );
+
+		_cacheMap.remove( entry.contextID() );
+
+		// Remove the parent's children from the global context->page map
+		entry.children().forEach( ( childContextID, childEntry ) -> {
+			_allEntries.remove( childEntry.contextID() );
+		} );
+
+		_allEntries.remove( entry.contextID() );
+		logger.debug( "Popped contextID '{}' from page cache", entry.contextID() );
 	}
 
 	/**
@@ -88,29 +115,47 @@ public class NGPageCache {
 	public NGComponent restorePageFromCache( final String contextID ) {
 		logger.debug( "Restoring page from cache with contextID: " + contextID );
 
-		final NGPageCacheEntry cacheEntry = _cacheMap.get( contextID );
+		final NGPageCacheEntry cacheEntry = _allEntries.get( contextID );
 
 		// FIXME: This is probably the place to throw a page restoration error instead // Hugi 2024-09-30
 		if( cacheEntry == null ) {
-			throw new IllegalStateException( "No cached page was found for the contextID '%s'".formatted( contextID ) );
+			throw new IllegalStateException( "No cached page was found for contextID '%s'".formatted( contextID ) );
 		}
 
 		return cacheEntry.page();
 	}
 
 	/**
-	 * Moves the page associated with the given contextID to the top of the page cache
+	 * In the case of a full page update, moves the entry to the top of the page cache.
+	 * In the case of a partial update, moves the parent entry to the top of the page cache.
 	 */
 	public void retainPageWithContextIDInCache( final String contextID ) {
 		logger.debug( "Retaining contextID {} in cache", contextID );
-		final NGPageCacheEntry cacheEntry = _cacheMap.remove( contextID );
 
-		// If we're attempting to retain a non-existent cache entry, odds are we're doing something bad so let's check for that.
-		if( cacheEntry == null ) {
-			throw new IllegalStateException( "Attempted to retain page cache entry for non-existent contextID '%s'. Probably not your fault, but the framework is doing something it shouldn't be doing".formatted( contextID ) );
+		final NGPageCacheEntry cacheEntry = _allEntries.get( contextID );
+
+		if( cacheEntry.isPartial() ) {
+			logger.debug( "contextID '{}' is partial, so we'll retain it's parent '{}' instead", cacheEntry.contextID(), cacheEntry.originatingContextID() );
+			final NGPageCacheEntry rootEntry = _cacheMap.get( cacheEntry.originatingContextID() );
+			retainRootEntry( rootEntry );
+		}
+		else {
+			retainRootEntry( cacheEntry );
+		}
+	}
+
+	/**
+	 * Retains the entry in the cache. Currently, only root entries can be retained, since child entries are retained for the lifetime of the parent
+	 */
+	private void retainRootEntry( NGPageCacheEntry cacheEntry ) {
+		final NGPageCacheEntry rootEntry = _cacheMap.remove( cacheEntry.contextID() );
+
+		// If we're attempting to retain a non-existent root entry, odds are we're doing something bad so let's check for that.
+		if( rootEntry == null ) {
+			throw new IllegalStateException( "Attempted to retain root page cache entry for non-existent contextID '%s'. Probably not your fault, but the framework is doing something it shouldn't be doing".formatted( cacheEntry.contextID() ) );
 		}
 
-		_cacheMap.put( contextID, cacheEntry );
+		_cacheMap.put( cacheEntry.contextID(), rootEntry );
 	}
 
 	/**
