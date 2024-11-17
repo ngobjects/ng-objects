c7f18610fec4644e1bbddd11d3cedd62faa17063
Hugi Thordarson <hugi@karlmenn.is>
2024-11-17 18:42:49 +0000

    Hide internals of PBasicNode from outside world

--- ng-appserver/src/main/java/ng/appserver/templating/NGTemplateParserProxy.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGTemplateParserProxy.java
@@ -20,7 +20,7 @@ import ng.appserver.templating.NGDeclaration.NGBindingValue;
 import x.junk.NGElementNotFoundElement;
 
 /**
- * Serves as a bridge between the "new and old world" for template parsing
+ * Bridges the "new and old world" for template parsing
  */
 
 public class NGTemplateParserProxy {
@@ -28,6 +28,10 @@ public class NGTemplateParserProxy {
 	private final String _htmlString;
 	private final String _wodString;
 
+	/**
+	 * @param htmlString The HTML to parse
+	 * @param wodString The associated wod/declarations
+	 */
 	public NGTemplateParserProxy( String htmlString, String wodString ) {
 		Objects.requireNonNull( htmlString );
 		Objects.requireNonNull( wodString );
@@ -36,15 +40,18 @@ public class NGTemplateParserProxy {
 		_wodString = wodString;
 	}
 
+	/**
+	 * @return A parsed element template
+	 */
 	public NGElement parse() throws NGDeclarationFormatException, NGHTMLFormatException {
-		final PNode pNode = new NGTemplateParser( _htmlString, _wodString ).parse();
-		return toDynamicElement( pNode );
+		final PNode rootNode = new NGTemplateParser( _htmlString, _wodString ).parse();
+		return toDynamicElement( rootNode );
 	}
 
 	private static NGElement toDynamicElement( final PNode node ) {
 		return switch( node ) {
 			case PBasicNode n -> toDynamicElement( n.tag() );
-			case PGroupNode n -> childrenToTemplate( n.children() );
+			case PGroupNode n -> toTemplate( n.children() );
 			case PHTMLNode n -> new NGHTMLBareString( n.value() );
 			case PCommentNode n -> new NGHTMLCommentString( n.value() );
 		};
@@ -52,13 +59,11 @@ public class NGTemplateParserProxy {
 
 	private static NGElement toDynamicElement( final NGDynamicHTMLTag tag ) {
 		try {
-			return NGApplication.dynamicElementWithName( tag.declaration().type(), associationsFromDeclaration( tag.declaration() ), childrenToTemplate( tag.childrenWithStringsProcessedAndCombined() ), Collections.emptyList() );
+			return NGApplication.dynamicElementWithName( tag.declaration().type(), associationsFromDeclaration( tag.declaration() ), toTemplate( tag.childrenWithStringsProcessedAndCombined() ), Collections.emptyList() );
 		}
 		catch( NGElementNotFoundException e ) {
-			// FIXME:
-			// Very experimental functionality at the moment and definitely does not belong with the parser part of the framework.
-			// But since it's definitely something we want to add ad some point, I'm keeping it for reference
-			// Hugi 2024-10-19
+			// FIXME: Experimental functionality, probably doesn't belong with the parser part of the framework.
+			// But since it's definitely something we want, I'm keeping this here for reference until it finds it's final home. // Hugi 2024-10-19
 			return new NGElementNotFoundElement( tag.declaration().type() );
 		}
 	}
@@ -74,25 +79,25 @@ public class NGTemplateParserProxy {
 	}
 
 	/**
-	 * @return The tag's template
+	 * @return An element/template from the given list of nodes.
 	 */
-	private static NGElement childrenToTemplate( final List<PNode> children ) {
+	private static NGElement toTemplate( final List<PNode> nodes ) {
 
-		// FIXME: Children should never really be null. I'm still hesitant to replace it with an empty list though, since in my mind that represents an empty container tag. Food for thought... // Hugi 2024-11-15
-		if( children == null ) {
+		// FIXME: Shouldn't really ever be null. Still hesitant to replace it with an empty list though, since in my mind that represents an empty container tag. Food for thought... // Hugi 2024-11-15
+		if( nodes == null ) {
 			return null;
 		}
 
-		final List<NGElement> childElements = new ArrayList<>();
+		final List<NGElement> elements = new ArrayList<>();
 
-		for( final PNode pNode : children ) {
-			childElements.add( toDynamicElement( pNode ) );
+		for( final PNode pNode : nodes ) {
+			elements.add( toDynamicElement( pNode ) );
 		}
 
-		if( childElements.size() == 1 ) {
-			return childElements.getFirst();
+		if( elements.size() == 1 ) {
+			return elements.getFirst();
 		}
 
-		return NGDynamicGroup.of( childElements );
+		return NGDynamicGroup.of( elements );
 	}
 }
