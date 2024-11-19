446c5fd7ec0e8d26f7d63f25c66250cc636d3f96
Hugi Thordarson <hugi@karlmenn.is>
2024-11-19 11:03:42 +0000

    Move inline error reporting to a generic separate error element

--- ng-appserver/src/main/java/ng/appserver/templating/NGTemplateParserProxy.java
+++ ng-appserver/src/main/java/ng/appserver/templating/NGTemplateParserProxy.java
@@ -14,14 +14,13 @@ import ng.appserver.NGApplication.NGElementNotFoundException;
 import ng.appserver.NGAssociation;
 import ng.appserver.NGAssociationFactory;
 import ng.appserver.NGBindingConfigurationException;
-import ng.appserver.NGContext;
 import ng.appserver.NGElement;
-import ng.appserver.NGResponse;
 import ng.appserver.elements.NGDynamicGroup;
 import ng.appserver.elements.NGHTMLBareString;
 import ng.appserver.elements.NGHTMLCommentString;
 import ng.appserver.templating.NGDeclaration.NGBindingValue;
 import x.junk.NGElementNotFoundElement;
+import x.junk.NGErrorMessageElement;
 
 /**
  * Bridges the "new and old world" for template parsing
@@ -80,15 +79,7 @@ public class NGTemplateParserProxy {
 			// FIXME: Digging this deep to get to the actual exception is crazy. We need to fix this up // Hugi 2024-11-19
 			if( e.getCause() instanceof InvocationTargetException ite ) {
 				if( ite.getCause() instanceof NGBindingConfigurationException bce ) {
-					return new NGElement() {
-
-						@Override
-						public void appendToResponse( NGResponse response, NGContext context ) {
-							response.appendContentString( """
-									<span style="display: inline-block; margin: 10px; padding: 10px; color:white; background-color: red; border-radius: 5px; box-shadow: 5px 5px 0px rgba(0,0,200,0.8); font-size: 12px;">Binding configuration error <br><span style="font-size: 16px"><strong>%s</strong><br>%s</span>
-									""".formatted( "&lt;wo:" + type + "&gt;", bce.getMessage() ) );
-						};
-					};
+					return new NGErrorMessageElement( "Binding configuration error", "&lt;wo:" + type + "&gt;", bce.getMessage() );
 				}
 			}
 
---
+++ ng-appserver/src/main/java/x/junk/NGErrorMessageElement.java
@@ -0,0 +1,25 @@
+package x.junk;
+
+import ng.appserver.NGContext;
+import ng.appserver.NGElement;
+import ng.appserver.NGResponse;
+
+public class NGErrorMessageElement implements NGElement {
+
+	private final String _type;
+	private final String _heading;
+	private final String _message;
+
+	public NGErrorMessageElement( final String type, final String heading, final String message ) {
+		_type = type;
+		_heading = heading;
+		_message = message;
+	}
+
+	@Override
+	public void appendToResponse( NGResponse response, NGContext context ) {
+		response.appendContentString( """
+				<span style="display: inline-block; margin: 10px; padding: 10px; color:white; background-color: red; border-radius: 5px; box-shadow: 5px 5px 0px rgba(0,0,200,0.8); font-size: 12px;">%s<br><span style="font-size: 16px"><strong>%s</strong><br>%s</span>
+				""".formatted( _type, _heading, _message ) );
+	}
+}
