334da07d3a77f8cbaef84dcef251b8ebd098f802
Hugi Thordarson <hugi@karlmenn.is>
2024-11-20 01:06:39 +0000

    Nicer look for errors

--- ng-appserver/src/main/java/x/junk/NGErrorMessageElement.java
+++ ng-appserver/src/main/java/x/junk/NGErrorMessageElement.java
@@ -20,10 +20,18 @@ public class NGErrorMessageElement implements NGElement {
 	public void appendToResponse( NGResponse response, NGContext context ) {
 		response.appendContentString( """
 				<span style="display: inline-block; margin: 10px; padding: 10px; color:white; background-color: rgba(255,100,100,0.5); border: 1px solid red; border-radius: 5px; box-shadow: 5px 5px 0px rgba(0,0,200,0.8); font-size: 12px;">
-					%s<br>
+					<span style="font-size: 20px">%s</span> %s<br>
 					<span style="font-size: 16px"><strong>%s</strong></span><br>
 					%s
 				</span>
-				""".formatted( _type, _heading, _message ) );
+				""".formatted( u( "DOG FACE" ), _type, _heading, _message ) );
+	}
+
+	/**
+	 * @return The unicode character corresponding to the given character name
+	 */
+	private static String u( final String unicodeCharacterName ) {
+		final int codePoint = Character.codePointOf( unicodeCharacterName );
+		return Character.toString( codePoint ).toString();
 	}
 }
