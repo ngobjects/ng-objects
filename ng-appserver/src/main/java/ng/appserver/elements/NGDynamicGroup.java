a8c2d4c9b028b4e09d2adcdeac4cb484481607e8
Hugi Thordarson <hugi@karlmenn.is>
2025-02-01 08:25:18 +0000

    Parser no longer inserts null content templates. FIXME removed

--- ng-appserver/src/main/java/ng/appserver/elements/NGDynamicGroup.java
+++ ng-appserver/src/main/java/ng/appserver/elements/NGDynamicGroup.java
@@ -154,12 +154,6 @@ public class NGDynamicGroup extends NGDynamicElement implements NGStructuralElem
 	 */
 	private static List<NGElement> childrenFromTemplate( final NGElement contentTemplate ) {
 
-		// Null represents an empty container tag (i.e. no children).
-		// FIXME: Eliminate this check. The rendering engine should be using an empty list. Disabled on 2024-11-18 // Hugi
-		//		if( contentTemplate == null ) {
-		//			return Collections.emptyList();
-		//		}
-
 		// If the contained template is another DynamicGroup, "unwrap it", i.e. just steal it's kids.
 		if( contentTemplate.getClass().equals( NGDynamicGroup.class ) ) {
 			return ((NGDynamicGroup)contentTemplate).children();
