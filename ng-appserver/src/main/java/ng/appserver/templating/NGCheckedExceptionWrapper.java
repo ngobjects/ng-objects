66dca0d6dabf051e77954166f9065b35ac332fed
Hugi Thordarson <hugi@karlmenn.is>
2025-03-08 16:52:22 +0000

    Create wrapper class for throwing checked exceptions from rendering

---
+++ ng-appserver/src/main/java/ng/appserver/templating/NGCheckedExceptionWrapper.java
@@ -0,0 +1,13 @@
+package ng.appserver.templating;
+
+/**
+ * Primarily intended for wrapping checked exceptions thrown during the rendering process.
+ * We need to be able to allow to throw that exception so it can pass up the stack for display in the UI.
+ */
+
+public class NGCheckedExceptionWrapper extends RuntimeException {
+
+	public NGCheckedExceptionWrapper( Throwable e ) {
+		super( e );
+	}
+}
