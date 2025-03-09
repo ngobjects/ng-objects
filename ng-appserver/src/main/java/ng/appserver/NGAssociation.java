2b8f1864bfc3e4a5987be91ff66cc8818a4c4f33
Hugi Thordarson <hugi@karlmenn.is>
2025-03-09 10:40:04 +0000

    Introduced NGAssociation.booleanValueInComponent()

--- ng-appserver/src/main/java/ng/appserver/NGAssociation.java
+++ ng-appserver/src/main/java/ng/appserver/NGAssociation.java
@@ -1,10 +1,26 @@
 package ng.appserver;
 
+import ng.appserver.privates._NGUtilities;
+
 public abstract class NGAssociation {
 
+	/**
+	 * @return The value of this association resolved against the given component
+	 */
 	public Object valueInComponent( NGComponent aComponent ) {
 		return null;
 	}
 
+	/**
+	 * Set the value of this association in the given component
+	 */
 	public void setValue( Object aValue, NGComponent aComponent ) {}
+
+	/**
+	 * @return The value of this binding interpreted as a boolean in a "truthy" way (as specified by isTruthy())
+	 */
+	public boolean booleanValueInComponent( NGComponent component ) {
+		final Object value = valueInComponent( component );
+		return _NGUtilities.isTruthy( value );
+	}
 }
