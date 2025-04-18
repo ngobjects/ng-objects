56df06903e34e77271b2ab05024e5f3a47aae414
Hugi Thordarson <hugi@karlmenn.is>
2025-04-18 15:17:38 +0000

    Added NGPlugin.dependsOn()

--- ng-appserver/src/main/java/ng/plugins/NGPlugin.java
+++ ng-appserver/src/main/java/ng/plugins/NGPlugin.java
@@ -1,5 +1,8 @@
 package ng.plugins;
 
+import java.util.Collections;
+import java.util.List;
+
 import ng.appserver.NGApplication;
 
 /**
@@ -14,14 +17,25 @@ public interface NGPlugin {
 	public default void load( NGApplication application ) {}
 
 	/**
-	 * The module's namespace. Defaults to the class namespace.
+	 * @return The plugin's namespace. Defaults to the class's simple name.
+	 *
 	 * The given namespace will be used for locating resources and elements defined by this module.
 	 */
 	public default String namespace() {
 		return getClass().getSimpleName();
 	}
 
+	/**
+	 * @return Instructions on how to locate elements provided by this plugin
+	 */
 	public default Elements elements() {
 		return Elements.create();
+	}
+
+	/**
+	 * @return A list of plugin classes that this plugin requires (and potentially depends on being loaded and initialized before itself)
+	 */
+	public default List<NGPlugin> dependsOn() {
+		return Collections.emptyList();
 	}
 }
