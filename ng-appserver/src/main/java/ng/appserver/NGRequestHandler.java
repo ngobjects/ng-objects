ea255c0dffe0f9bcbe732c97c18bc24383eeccc5
Hugi Thordarson <hugi@karlmenn.is>
2025-04-19 09:36:42 +0000

    Add note on NGRequestHandler really being an interface

--- ng-appserver/src/main/java/ng/appserver/NGRequestHandler.java
+++ ng-appserver/src/main/java/ng/appserver/NGRequestHandler.java
@@ -1,5 +1,9 @@
 package ng.appserver;
 
+/**
+ * FIXME: This should really be an interface // Hugi 2025-04-19
+ */
+
 public abstract class NGRequestHandler {
 
 	public abstract NGResponse handleRequest( NGRequest request );
