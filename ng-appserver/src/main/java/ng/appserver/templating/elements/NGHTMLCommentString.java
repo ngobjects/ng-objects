c52e897e4ca58a7cf13d42bc23748668d58a3641
Hugi Thordarson <hugi@karlmenn.is>
2025-08-19 08:14:50 +0000

    NGHTMLCommentString curr. doesn't need to override appendToResponse()

--- ng-appserver/src/main/java/ng/appserver/templating/elements/NGHTMLCommentString.java
+++ ng-appserver/src/main/java/ng/appserver/templating/elements/NGHTMLCommentString.java
@@ -1,8 +1,5 @@
 package ng.appserver.templating.elements;
 
-import ng.appserver.NGContext;
-import ng.appserver.NGResponse;
-
 /**
  * Represents a comment string.
  */
@@ -11,10 +8,5 @@ public class NGHTMLCommentString extends NGHTMLBareString {
 
 	public NGHTMLCommentString( String aString ) {
 		super( aString );
-	}
-
-	@Override
-	public void appendToResponse( NGResponse aResponse, NGContext aContext ) {
-		super.appendToResponse( aResponse, aContext );
 	}
 }
