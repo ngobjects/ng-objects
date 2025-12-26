843a18a2c7984a9d3265cc46fb5ff714022659d6
Hugi Thordarson <hugi@karlmenn.is>
2025-12-26 22:34:55 +0000

    Move Association Factory test to correctly named association package

--- ng-appserver/src/test/java/ng/appserver/templating/associations/TestNGAssociationFactory.java
+++ ng-appserver/src/test/java/ng/appserver/templating/associations/TestNGAssociationFactory.java
@@ -1,4 +1,4 @@
-package ng.appserver.templating.assications;
+package ng.appserver.templating.associations;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
