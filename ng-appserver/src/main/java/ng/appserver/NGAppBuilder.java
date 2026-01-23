3db9c208a4487b438a917604b63d28757355e412
Hugi Thordarson <hugi@karlmenn.is>
2026-01-23 11:51:01 +0000

    Start planning for our actual initialization

---
+++ ng-appserver/src/main/java/ng/appserver/NGAppBuilder.java
@@ -0,0 +1,16 @@
+package ng.appserver;
+
+/// # A builder
+///
+/// We need a builder class to create/run our applications.
+/// What I'd b love to have is
+///
+/// 1. Build an application without running it
+/// 2. Specify the application's deployment mode
+/// 3. Specify which plugins to load
+///  - Specify whether to automatically load all located plugins
+///  - Disable certain plugins
+///  - Change plugin initialization/load/activation order?
+/// 4. Launch the given application with a specified "adaptor" (might potentially belong in a separate class?)
+
+public class NGAppBuilder {}
