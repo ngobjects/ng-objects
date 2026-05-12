77f4f841069ac372ea1fefb5cd8b6d8c8786a365
Hugi Thordarson <hugi@karlmenn.is>
2026-05-12 15:35:11 +0000

    Cleanup in var naming in NGMessage

--- ng-appserver/src/main/java/ng/appserver/NGMessage.java
+++ ng-appserver/src/main/java/ng/appserver/NGMessage.java
@@ -40,7 +40,7 @@ public abstract class NGMessage implements NGMessageInterface {
 	 * For example, it's clear that using a StringBuilder for string responses is significantly more efficient than using the ByteArrayOutputStream
 	 * // Hugi 2023-02-08
 	 */
-	private ByteArrayOutputStream _contentByteOutputStream = new ByteArrayOutputStream( DEFAULT_CONTENT_DATA_LENGTH );
+	private ByteArrayOutputStream _contentByteStream = new ByteArrayOutputStream( DEFAULT_CONTENT_DATA_LENGTH );
 
 	/**
 	 * @return The HTTP version of this message
@@ -148,31 +148,35 @@ public abstract class NGMessage implements NGMessageInterface {
 	}
 
 	public byte[] contentBytes() {
-		return _contentByteOutputStream.toByteArray();
+		return contentByteStream().toByteArray();
 	}
 
 	/**
 	 * @return The length of the message's data content
 	 */
 	public long contentBytesLength() {
-		return _contentByteOutputStream.size();
+		return contentByteStream().size();
 	}
 
 	/**
 	 * @return The response's content stream
 	 */
 	public ByteArrayOutputStream contentByteStream() {
-		return _contentByteOutputStream;
+		return _contentByteStream;
+	}
+
+	public void _setContentByteStream( ByteArrayOutputStream value ) {
+		_contentByteStream = value;
 	}
 
 	public void setContentBytes( final byte[] contentBytes ) {
-		_contentByteOutputStream = new ByteArrayOutputStream( DEFAULT_CONTENT_DATA_LENGTH );
+		_setContentByteStream( new ByteArrayOutputStream( DEFAULT_CONTENT_DATA_LENGTH ) );
 		appendContentBytes( contentBytes );
 	}
 
 	private void appendContentBytes( final byte[] contentBytes ) {
 		try {
-			_contentByteOutputStream.write( contentBytes );
+			contentByteStream().write( contentBytes );
 		}
 		catch( IOException e ) {
 			throw new UncheckedIOException( e );
