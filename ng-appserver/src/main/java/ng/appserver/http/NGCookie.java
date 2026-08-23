039d09cd4f98b03bf35495453ccdc9992d874958
Hugi Thordarson <hugi@karlmenn.is>
2026-08-23 10:11:53 +0000

    NGCookie converted to a record

--- ng-appserver/src/main/java/ng/appserver/http/NGCookie.java
+++ ng-appserver/src/main/java/ng/appserver/http/NGCookie.java
@@ -6,34 +6,16 @@ import java.util.Objects;
  * A cookie. Yum!
  */
 
-public class NGCookie {
+public record NGCookie( String name, String value, String domain, String path, Long maxAge, boolean secure, boolean httpOnly, String sameSite ) {
 
-	private String _name;
-	private String _value;
-	private String _domain;
-	private String _path;
-	private Long _maxAge;
-	private boolean _secure;
-	private boolean _httpOnly;
-	private String _sameSite; // Strict,Lax,None
-
-	public NGCookie( String name, String value, String domain, String path, Long maxAge, boolean isSecure, boolean isHttpOnly, String sameSite ) {
+	public NGCookie {
 		Objects.requireNonNull( name, "A cookie's [name] must not be null" );
 		Objects.requireNonNull( value, "A cookie's [value] must not be null" );
 
 		// Browsers reject SameSite=None cookies that aren't Secure, so failing at construction beats failing silently in the user's cookie jar
-		if( "None".equals( sameSite ) && !isSecure ) {
+		if( "None".equals( sameSite ) && !secure ) {
 			throw new IllegalArgumentException( "A cookie with SameSite=None must also be secure (browsers reject the combination otherwise)" );
 		}
-
-		_name = name;
-		_value = value;
-		_domain = domain;
-		_path = path;
-		_maxAge = maxAge;
-		_secure = isSecure;
-		_httpOnly = isHttpOnly;
-		_sameSite = sameSite;
 	}
 
 	public NGCookie( final String name, final String value, final Long maxAge ) {
@@ -42,57 +24,5 @@ public class NGCookie {
 
 	public NGCookie( final String name, final String value ) {
 		this( name, value, null );
-	}
-
-	public String name() {
-		return _name;
-	}
-
-	public String value() {
-		return _value;
-	}
-
-	public String domain() {
-		return _domain;
-	}
-
-	public String path() {
-		return _path;
-	}
-
-	public void setPath( String path ) {
-		_path = path;
-	}
-
-	public Long maxAge() {
-		return _maxAge;
-	}
-
-	public void setMaxAge( Long maxAge ) {
-		_maxAge = maxAge;
-	}
-
-	public boolean secure() {
-		return _secure;
-	}
-
-	public void setSecure( boolean secure ) {
-		_secure = secure;
-	}
-
-	public boolean httpOnly() {
-		return _httpOnly;
-	}
-
-	public void setHttpOnly( boolean httpOnly ) {
-		_httpOnly = httpOnly;
-	}
-
-	public String sameSite() {
-		return _sameSite;
-	}
-
-	public void setSameSite( String value ) {
-		_sameSite = value;
 	}
 }
